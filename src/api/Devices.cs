b5c3fb221241b81ffd8eb29e4fbe3348984cc9cb
mat1jaczyyy <mat1jaczyyy@gmail.com>
2018-08-22 15:44:38 +0200

    Implement Chord effect (for only 1 offset)

--- src/api/Devices.cs
+++ src/api/Devices.cs
@@ -43,4 +43,41 @@ namespace api.Devices {
             this.MIDIExit(n);
         }
     }
+
+    public class Chord: Device {
+        // Note offset
+        private int _offset;
+
+        public int Offset {
+            get {
+                return _offset;
+            }
+            set {
+                if (-128 <= _offset && _offset <= 128) _offset = value;
+            }
+        }
+
+        // Create device
+        public Chord(Action<Communication.Note> exit) {
+            this.Offset = 0;
+            this.MIDIExit = exit;
+        }
+
+        // Load device
+        public Chord(int offset, Action<Communication.Note> exit) {
+            this.Offset = offset;
+            this.MIDIExit = exit;
+        }
+
+        public override void MIDIEnter(Communication.Note n) {
+            this.MIDIExit(n);
+            
+            n.p += _offset;
+            
+            if (n.p < 0) n.p = 0;
+            if (n.p > 127) n.p = 127;
+
+            this.MIDIExit(n);
+        }
+    }
 }
