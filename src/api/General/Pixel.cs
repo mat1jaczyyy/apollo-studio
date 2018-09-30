a6ddcba8dc93c07b0962b4a5f0257492cb97dbb2
mat1jaczyyy <mat1jaczyyy@gmail.com>
2018-09-30 20:23:37 +0200

    Pixel: Lock execution to single thread

--- src/api/General/Pixel.cs
+++ src/api/General/Pixel.cs
@@ -7,6 +7,7 @@ namespace api {
         private SortedList<int, Signal> _signals = new SortedList<int, Signal>();
         private int? _highest = null;
         public Action<Signal> MIDIExit = null;
+        private object locker = new object();
 
         public Pixel() {}
 
@@ -15,44 +16,46 @@ namespace api {
         }
 
         public void MIDIEnter(Signal n) {
-            int layer = -n.Layer;
+            lock (locker) {
+                int layer = -n.Layer;
 
-            if (_signals.ContainsKey(layer)) {
-                if (n.Color.Lit) {
-                    _signals[layer] = n;
-                    
-                    if (layer == _highest)
-                        if (MIDIExit != null)
-                            MIDIExit(n);
-                
-                } else {
-                    _signals.Remove(layer);
-
-                    if (layer == _highest) {
-                        if (_signals.Count == 0) {
-                            _highest = null;
-
-                            if (MIDIExit != null)
-                                MIDIExit(n);
+                if (_signals.ContainsKey(layer)) {
+                    if (n.Color.Lit) {
+                        _signals[layer] = n.Clone();
                         
-                        } else {
-                            _highest = _signals.Keys[0];
-
+                        if (layer == _highest)
                             if (MIDIExit != null)
-                                MIDIExit(_signals[(int)_highest]);
+                                MIDIExit(n.Clone());
+                    
+                    } else {
+                        _signals.Remove(layer);
+
+                        if (layer == _highest) {
+                            if (_signals.Count == 0) {
+                                _highest = null;
+
+                                if (MIDIExit != null)
+                                    MIDIExit(n.Clone());
+                            
+                            } else {
+                                _highest = _signals.Keys[0];
+
+                                if (MIDIExit != null)
+                                    MIDIExit(_signals[(int)_highest].Clone());
+                            }
                         }
                     }
-                }
-            
-            } else {
-                if (n.Color.Lit) {
-                    _signals[layer] = n;
+                
+                } else {
+                    if (n.Color.Lit) {
+                        _signals[layer] = n.Clone();
 
-                    if (!_highest.HasValue || layer < _highest) {
-                        _highest = layer;
+                        if (!_highest.HasValue || layer < _highest) {
+                            _highest = layer;
 
-                        if (MIDIExit != null)
-                            MIDIExit(n);
+                            if (MIDIExit != null)
+                                MIDIExit(n.Clone());
+                        }
                     }
                 }
             }
