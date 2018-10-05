ded5c80aea957e3a8f022d5306e32a7484b8c2a0
vaaski <admin@vaa.ski>
2018-10-05 18:03:06 +0200

    replaced dial with a more lightweight one

---
+++ src/app/src/renderer/devices/translation.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+main
+  //- .x
+  //-   h4 x translation
+  //-   dial(:initialValue="0" @update:value="x = $event" :size="50" :width="5" :min="-8" :max="8")
+  //-   p {{x}}
+  .y
+    h4 y translation
+    dial(:initialValue="0" @update:value="y = $event" :size="50" :width="5" :min="0" :max="100")
+    p {{y}}
+</template>
+
+<script>
+import dial from "../ui/dial"
+export default {
+  components: { dial },
+  data: () => ({
+    x: 0,
+    y: 0,
+  }),
+}
+</script>
+
+<style lang="scss">
+.x,
+.y {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 5px;
+  h4 {
+    margin-bottom: 15px;
+  }
+}
+</style>
