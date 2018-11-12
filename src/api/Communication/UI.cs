541d432a14dd08ab1bf9daa69febb21a6da574a5
mat1jaczyyy <mat1jaczyyy@gmail.com>
2018-11-12 20:44:36 +0100

    UI: Remove unnecessary using statement

--- src/api/Communication/UI.cs
+++ src/api/Communication/UI.cs
@@ -9,7 +9,7 @@ namespace api.Communication {
 
         public static async void Init() {
             using (HttpClient client = new HttpClient())
-                using (HttpResponseMessage res = await client.PostAsync($"http://{ip}:{port}/init", new StringContent(Set.Encode(), Encoding.UTF8, "application/json")));
+                await client.PostAsync($"http://{ip}:{port}/init", new StringContent(Set.Encode(), Encoding.UTF8, "application/json"));
         }
     }
 }
