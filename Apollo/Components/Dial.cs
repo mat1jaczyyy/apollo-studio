7b4b0c209cec21765da4731247e56483f28ea420
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-03-15 13:51:45 +0100

    Create Dial, add to Track for testing

---
+++ Apollo/Components/Dial.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Apollo.Components {
+    public class Dial: UserControl {
+        public Dial() {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
