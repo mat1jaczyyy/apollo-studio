9973c4cf909387a58774380cd3cb836223cc0f80
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-03-19 00:20:52 +0100

    Implement DelayViewer

---
+++ Apollo/DeviceViewers/DelayViewer.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+using Apollo.Devices;
+
+namespace Apollo.DeviceViewers {
+    public class DelayViewer: UserControl {
+        public static readonly string DeviceIdentifier = "delay";
+
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+        
+        Delay _device;
+
+        public DelayViewer(Delay device) {
+            InitializeComponent();
+
+            _device = device;
+        }
+    }
+}
