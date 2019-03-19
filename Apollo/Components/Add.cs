0d3d14586ad865df266e815d48079dd301c9ce3b
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-03-19 13:15:17 +0100

    Implement add Device button with ContextMenu

---
+++ Apollo/Components/Add.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Apollo.Components {
+    public class Add: UserControl {
+        public static List<string> devices = new List<string>() {
+            "Delay"
+        };
+
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+
+        public Add() {
+            InitializeComponent();
+
+            this.Get<Canvas>("AddCanvas").ContextMenu = new ContextMenu() {
+                Items = devices
+            };
+        }
+    }
+}
