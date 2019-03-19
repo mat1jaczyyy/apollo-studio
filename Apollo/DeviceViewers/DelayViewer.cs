8532e68245f3b57a7e154d6103b3571e10e8050d
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-03-19 01:15:07 +0100

    Delay: Initialize Dials with values from Device

--- Apollo/DeviceViewers/DelayViewer.cs
+++ Apollo/DeviceViewers/DelayViewer.cs
@@ -4,6 +4,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
+using Apollo.Components;
 using Apollo.Devices;
 
 namespace Apollo.DeviceViewers {
@@ -12,20 +13,22 @@ namespace Apollo.DeviceViewers {
 
         private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
         
-        Delay _device;
+        Delay _delay;
 
-        public DelayViewer(Delay device) {
+        public DelayViewer(Delay delay) {
             InitializeComponent();
 
-            _device = device;
+            _delay = delay;
+            this.Get<Dial>("Duration").RawValue = _delay.Time;
+            this.Get<Dial>("Gate").RawValue = (double)_delay.Gate * 100;
         }
 
         private void Duration_Changed(double value) {
-            _device.Time = (int)value;
+            _delay.Time = (int)value;
         }
 
         private void Gate_Changed(double value) {
-            _device.Gate = (decimal)(value / 100);
+            _delay.Gate = (decimal)(value / 100);
         }
     }
 }
