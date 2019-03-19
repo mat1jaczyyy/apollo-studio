c06fcd282d4fbcb5bd0de489c189a7f86bc66351
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-03-19 23:07:31 +0100

    Add: Implement DeviceAdded event

--- Apollo/Components/Add.cs
+++ Apollo/Components/Add.cs
@@ -1,23 +1,36 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+
+using Apollo.Devices;
+using Apollo.Elements;
 
 namespace Apollo.Components {
     public class Add: UserControl {
-        public static List<string> devices = new List<string>() {
-            "Delay"
-        };
-
         private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+
+        public delegate void DeviceAddedEventHandler(Type device);
+        public event DeviceAddedEventHandler DeviceAdded;
 
         public Add() {
             InitializeComponent();
 
-            this.Get<Canvas>("AddCanvas").ContextMenu = new ContextMenu() {
-                Items = devices
-            };
+            Canvas AddCanvas = this.Get<Canvas>("AddCanvas");
+            AddCanvas.ContextMenu.AddHandler(MenuItem.ClickEvent, new EventHandler(ContextMenu_Click));
+        }
+
+        private void ContextMenu_Click(object _, EventArgs e) {
+            IInteractive sender = ((RoutedEventArgs)e).Source;
+
+            if (sender.GetType() == typeof(MenuItem)) {
+                string selected = ((MenuItem)sender).Header.ToString();
+                DeviceAdded?.Invoke(Assembly.GetExecutingAssembly().GetType($"Apollo.Devices.{selected}"));
+            }
         }
     }
 }
