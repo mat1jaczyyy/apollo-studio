9e8fe10201430b3ff0e2e383c1885b56305ab531
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-03-26 18:30:38 +0100

    Implement HoldViewer

---
+++ Apollo/DeviceViewers/HoldViewer.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+using Apollo.Components;
+using Apollo.Devices;
+
+namespace Apollo.DeviceViewers {
+    public class HoldViewer: UserControl {
+        public static readonly string DeviceIdentifier = "hold";
+
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+        
+        Hold _hold;
+
+        public HoldViewer(Hold hold) {
+            InitializeComponent();
+
+            _hold = hold;
+            this.Get<Dial>("Duration").RawValue = _hold.Time;
+            this.Get<Dial>("Gate").RawValue = (double)_hold.Gate * 100;
+        }
+
+        private void Duration_Changed(double value) {
+            _hold.Time = (int)value;
+        }
+
+        private void Gate_Changed(double value) {
+            _hold.Gate = (decimal)(value / 100);
+        }
+    }
+}
