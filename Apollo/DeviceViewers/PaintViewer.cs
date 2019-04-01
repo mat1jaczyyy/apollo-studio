ed77789fac1a1e06ae270222f876488682cae541
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-04-01 14:04:42 +0200

    PaintViewer: Update Color on Paint device

--- Apollo/DeviceViewers/PaintViewer.cs
+++ Apollo/DeviceViewers/PaintViewer.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia.Controls;
+﻿using System;
+
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Shapes;
 using Avalonia.Input;
@@ -6,6 +8,7 @@ using Avalonia.Markup.Xaml;
 
 using Apollo.Components;
 using Apollo.Devices;
+using Apollo.Structures;
 
 namespace Apollo.DeviceViewers {
     public class PaintViewer: UserControl {
@@ -15,6 +18,7 @@ namespace Apollo.DeviceViewers {
         
         Paint _paint;
         Ellipse color;
+        Thumb mainThumb, hueThumb;
 
         public PaintViewer(Paint paint) {
             InitializeComponent();
@@ -23,6 +27,30 @@ namespace Apollo.DeviceViewers {
             
             color = this.Get<Ellipse>("Color");
             color.Fill = _paint.Color.ToBrush();
+
+            mainThumb = this.Get<Thumb>("MainThumb");
+            hueThumb = this.Get<Thumb>("HueThumb");
+        }
+
+        private void UpdateColor() {
+            double hue = Canvas.GetLeft(hueThumb) * 360 / ((Canvas)hueThumb.Parent).Bounds.Width;
+            double saturation = Canvas.GetLeft(mainThumb) / ((Canvas)mainThumb.Parent).Bounds.Width;
+            double value = (1 - (Canvas.GetTop(mainThumb) / ((Canvas)mainThumb.Parent).Bounds.Height)) * 63;
+
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+
+            byte v = Convert.ToByte(value);
+            byte p = Convert.ToByte(value * (1 - saturation));
+            byte q = Convert.ToByte(value * (1 - f * saturation));
+            byte t = Convert.ToByte(value * (1 - (1 - f) * saturation));
+
+            if (hi == 0)      _paint.Color = new Color(v, t, p);
+            else if (hi == 1) _paint.Color = new Color(q, v, p);
+            else if (hi == 2) _paint.Color = new Color(p, v, t);
+            else if (hi == 3) _paint.Color = new Color(p, q, v);
+            else if (hi == 4) _paint.Color = new Color(t, p, v);
+            else              _paint.Color = new Color(v, p, q);
         }
 
         private void MainThumbMove(object sender, VectorEventArgs e) {
@@ -34,6 +62,8 @@ namespace Apollo.DeviceViewers {
 
             if (0 <= x && x <= Area.Bounds.Width) Canvas.SetLeft(thumb, x);
             if (0 <= y && y <= Area.Bounds.Height) Canvas.SetTop(thumb, y);
+
+            UpdateColor();
         }
 
         private void HueThumbMove(object sender, VectorEventArgs e) {
@@ -42,6 +72,8 @@ namespace Apollo.DeviceViewers {
 
             double x = Canvas.GetLeft(thumb) + e.Vector.X;
             if (0 <= x && x <= Area.Bounds.Width) Canvas.SetLeft(thumb, x);
+
+            UpdateColor();
         }
     }
 }
