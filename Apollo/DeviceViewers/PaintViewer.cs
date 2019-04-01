cac973b5a3bad82a6b2aaa229969a086ba6816c3
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-04-01 17:36:56 +0200

    Paint: Initialize Canvas

--- Apollo/DeviceViewers/PaintViewer.cs
+++ Apollo/DeviceViewers/PaintViewer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Shapes;
@@ -7,10 +9,13 @@ using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using AvaloniaColor = Avalonia.Media.Color;
 using GradientStop = Avalonia.Media.GradientStop;
+using Avalonia.VisualTree;
 
+using Apollo.Core;
 using Apollo.Components;
 using Apollo.Devices;
 using Apollo.Structures;
+using Apollo.Windows;
 
 namespace Apollo.DeviceViewers {
     public class PaintViewer: UserControl {
@@ -30,12 +35,58 @@ namespace Apollo.DeviceViewers {
             _paint = paint;
             
             color = this.Get<Ellipse>("Color");
-            mainColor = this.Get<GradientStop>("MainColor");
-
             color.Fill = _paint.Color.ToBrush();
 
             mainThumb = this.Get<Thumb>("MainThumb");
             hueThumb = this.Get<Thumb>("HueThumb");
+
+            mainColor = this.Get<GradientStop>("MainColor");
+        }
+
+        private void InitCanvas(object sender, EventArgs e) {
+            double hueWidth = ((Canvas)hueThumb.Parent).Bounds.Width;
+            double mainWidth = ((Canvas)mainThumb.Parent).Bounds.Width;
+            double mainHeight = ((Canvas)mainThumb.Parent).Bounds.Height;
+
+            if (hueWidth == 0 || mainWidth == 0 || mainHeight == 0) return;
+
+            double r = _paint.Color.Red / 63.0;
+            double g = _paint.Color.Green / 63.0;
+            double b = _paint.Color.Blue / 63.0;
+            double[] color = new double[] {r, g, b};
+
+            double min = color.Min();
+            double max = color.Max();
+
+            double hue = 0;
+            if (min != max) {
+                double diff = max - min;
+
+                if (max == r) {
+                    hue = (g - b) / diff;
+                } else if (max == g) {
+                    hue = (b - r) / diff + 2.0;
+                } else if (max == b) {
+                    hue = (r - g) / diff + 4.0;
+                }
+                if (hue < 0) hue += 6.0;
+            }
+
+            double saturation = 0;
+            if (max != 0) saturation = 1 - (min / max);
+
+            Canvas.SetLeft(hueThumb, hue * ((Canvas)hueThumb.Parent).Bounds.Width / 6);
+            Canvas.SetLeft(mainThumb, saturation * ((Canvas)mainThumb.Parent).Bounds.Width);
+            Canvas.SetTop(mainThumb, (1 - max) * ((Canvas)mainThumb.Parent).Bounds.Height);
+
+            mainColor.Color = new AvaloniaColor(
+                255,
+                (byte)(r * 255),
+                (byte)(g * 255),
+                (byte)(b * 255)
+            );
+
+            ((Canvas)sender).LayoutUpdated -= InitCanvas;
         }
 
         private void UpdateColor() {
