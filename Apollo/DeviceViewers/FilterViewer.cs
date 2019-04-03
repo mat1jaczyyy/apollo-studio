ae15e4aa2c3d8bd7357e1f06c556fb6e18a14655
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-04-03 22:50:23 +0200

    FilterViewer: Load initial Device parameters

--- Apollo/DeviceViewers/FilterViewer.cs
+++ Apollo/DeviceViewers/FilterViewer.cs
@@ -20,20 +20,26 @@ namespace Apollo.DeviceViewers {
         Filter _filter;
         UniformGrid LaunchpadGrid;
 
+        private int GridToSignal(int index) => (index == -1)? 99 : ((9 - (index / 10)) * 10 + index % 10);
+
+        private void Set(Shape shape, bool value) => shape.Fill = (IBrush)Application.Current.Styles.FindResource(value? "ThemeAccentBrush" : "ThemeForegroundLowBrush");
+
         public FilterViewer(Filter filter) {
             InitializeComponent();
 
             _filter = filter;
 
             LaunchpadGrid = this.Get<UniformGrid>("LaunchpadGrid");
+
+            for (int i = 0; i < LaunchpadGrid.Children.Count; i++) {
+                int index = GridToSignal(i);
+                if (index != 0 && index != 9 && index != 90 && index != 99) Set((Shape)LaunchpadGrid.Children[i], _filter[index]);
+            }
         }
 
         private void Clicked(object sender, PointerReleasedEventArgs e) {
-            int index = LaunchpadGrid.Children.IndexOf((IControl)sender);
-            int result = (index == -1)? 99 : ((9 - (index / 10)) * 10 + index % 10);
-
-            _filter[result] = !_filter[result];
-            ((Shape)sender).Fill = (IBrush)Application.Current.Styles.FindResource(_filter[result]? "ThemeAccentBrush" : "ThemeForegroundLowBrush");
+            int index = GridToSignal(LaunchpadGrid.Children.IndexOf((IControl)sender));
+            Set((Shape)sender, _filter[index] = !_filter[index]);
         }
     }
 }
