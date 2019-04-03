20db8cfbffef6e4ff4354bf0acba6803902b19a2
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-04-03 22:25:24 +0200

    FilterViewer: Implement Launchpad Design

---
+++ Apollo/DeviceViewers/FilterViewer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Markup.Xaml;
+using Avalonia.Input;
+
+using Apollo.Components;
+using Apollo.Devices;
+
+namespace Apollo.DeviceViewers {
+    public class FilterViewer: UserControl {
+        public static readonly string DeviceIdentifier = "filter";
+
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+        
+        Filter _filter;
+
+        public FilterViewer(Filter filter) {
+            InitializeComponent();
+
+            _filter = filter;
+        }
+
+        private void Clicked(object sender, PointerReleasedEventArgs e) {
+            
+        }
+    }
+}
