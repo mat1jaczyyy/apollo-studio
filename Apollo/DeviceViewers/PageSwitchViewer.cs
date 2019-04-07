b7740cb477c16ac68d92276f101ae851527cc016
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-04-07 18:04:21 +0200

    Implement PageSwitchViewer

---
+++ Apollo/DeviceViewers/PageSwitchViewer.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+using Apollo.Components;
+using Apollo.Devices;
+
+namespace Apollo.DeviceViewers {
+    public class PageSwitchViewer: UserControl {
+        public static readonly string DeviceIdentifier = "pageswitch";
+
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+        
+        PageSwitch _pageswitch;
+
+        public PageSwitchViewer(PageSwitch pageswitch) {
+            InitializeComponent();
+
+            _pageswitch = pageswitch;
+            this.Get<Dial>("Target").RawValue = _pageswitch.Target;
+        }
+
+        private void Target_Changed(double value) => _pageswitch.Target = (int)value;
+    }
+}
