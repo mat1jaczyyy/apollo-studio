6b75c301ab804ecce08f6e6de5e49a1029772b17
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-04-07 18:20:59 +0200

    PageSwitch: Fix Target Range

--- Apollo/Devices/PageSwitch.cs
+++ Apollo/Devices/PageSwitch.cs
@@ -13,18 +13,18 @@ namespace Apollo.Devices {
     public class PageSwitch: Device {
         public static readonly new string DeviceIdentifier = "pageswitch";
 
-        private int _target = 0;
+        private int _target = 1;
         public int Target {
             get => _target;
             set {
-                if (0 <= value && value <= 99)
+                if (1 <= value && value <= 100)
                     _target = value;
             }
         }
 
         public override Device Clone() => new PageSwitch(Target);
 
-        public PageSwitch(int target = 0): base(DeviceIdentifier) => Target = target;
+        public PageSwitch(int target = 1): base(DeviceIdentifier) => Target = target;
 
         public override void MIDIEnter(Signal n) {
             Program.Project.Page = Target;
