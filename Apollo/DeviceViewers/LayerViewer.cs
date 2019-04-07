28058372f29cbece9183a0c540b1bbcced7e8f3a
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-04-07 18:32:53 +0200

    Implement LayerViewer

---
+++ Apollo/DeviceViewers/LayerViewer.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+using Apollo.Components;
+using Apollo.Devices;
+
+namespace Apollo.DeviceViewers {
+    public class LayerViewer: UserControl {
+        public static readonly string DeviceIdentifier = "layer";
+
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+        
+        Layer _layer;
+
+        public LayerViewer(Layer layer) {
+            InitializeComponent();
+
+            _layer = layer;
+            this.Get<Dial>("Target").RawValue = _layer.Target;
+        }
+
+        private void Target_Changed(double value) => _layer.Target = (int)value;
+    }
+}
