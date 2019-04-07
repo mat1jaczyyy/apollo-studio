0b680a89630021bda8b4411b3f95245d195044ae
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-04-07 17:59:13 +0200

    Implement PageSwitch

---
+++ Apollo/Devices/PageSwitch.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using Newtonsoft.Json;
+
+using Apollo.Core;
+using Apollo.Elements;
+using Apollo.Structures;
+
+namespace Apollo.Devices {
+    public class PageSwitch: Device {
+        public static readonly new string DeviceIdentifier = "pageswitch";
+
+        private int _target = 0;
+        public int Target {
+            get => _target;
+            set {
+                if (0 <= value && value <= 99)
+                    _target = value;
+            }
+        }
+
+        public override Device Clone() => new PageSwitch(Target);
+
+        public PageSwitch(int target = 0): base(DeviceIdentifier) => Target = target;
+
+        public override void MIDIEnter(Signal n) {
+            Program.Project.Page = Target;
+            MIDIExit?.Invoke(n);
+        }
+
+        public static Device DecodeSpecific(string jsonString) {
+            Dictionary<string, object> json = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonString);
+            if (json["device"].ToString() != DeviceIdentifier) return null;
+
+            Dictionary<string, object> data = JsonConvert.DeserializeObject<Dictionary<string, object>>(json["data"].ToString());
+            
+            return new PageSwitch(
+                Convert.ToInt32(data["target"])
+            );
+        }
+
+        public override string EncodeSpecific() {
+            StringBuilder json = new StringBuilder();
+
+            using (JsonWriter writer = new JsonTextWriter(new StringWriter(json))) {
+                writer.WriteStartObject();
+
+                    writer.WritePropertyName("device");
+                    writer.WriteValue(DeviceIdentifier);
+
+                    writer.WritePropertyName("data");
+                    writer.WriteStartObject();
+
+                        writer.WritePropertyName("target");
+                        writer.WriteValue(Target);
+
+                    writer.WriteEndObject();
+
+                writer.WriteEndObject();
+            }
+            
+            return json.ToString();
+        }
+    }
+}
