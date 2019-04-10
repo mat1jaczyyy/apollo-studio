359d9c5338f1f78e3b7b51d86816e3aa55f949d8
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-04-10 20:16:38 +0200

    Implement OffsetAdd

---
+++ Apollo/Components/OffsetAdd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace Apollo.Components {
+    public class OffsetAdd: UserControl {
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+
+        public delegate void OffsetAddedEventHandler();
+        public event OffsetAddedEventHandler OffsetAdded;
+
+        private bool _always;
+        public bool AlwaysShowing {
+            get => _always;
+            set {
+                if (value != _always) {
+                    _always = value;
+                    this.Get<Grid>("Root").MinWidth = _always? 22 : 0;
+                }
+            }
+        }
+
+        public OffsetAdd() => InitializeComponent();
+
+        private void Click(object sender, PointerReleasedEventArgs e) {
+            if (e.MouseButton == MouseButton.Left) OffsetAdded?.Invoke();
+        }
+    }
+}
