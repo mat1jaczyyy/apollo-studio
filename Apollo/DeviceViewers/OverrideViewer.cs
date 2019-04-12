ab88b0a1ad03e6574f3face2a7adb009cc7eecbf
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-04-12 14:05:17 +0200

    Implement OverrideViewer

---
+++ Apollo/DeviceViewers/OverrideViewer.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+using Apollo.Components;
+using Apollo.Devices;
+
+namespace Apollo.DeviceViewers {
+    public class OverrideViewer: UserControl {
+        public static readonly string DeviceIdentifier = "override";
+
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+        
+        Override _override;
+
+        public OverrideViewer(Override o) {
+            InitializeComponent();
+            
+            _override = o;
+            this.Get<Dial>("Target").RawValue = _override.Target;
+        }
+
+        private void Target_Changed(double value) => _override.Target = (int)value;
+    }
+}
