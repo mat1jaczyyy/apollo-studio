539129696b591d05413d88a383542111a2a6841a
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-04-19 23:53:17 +0200

    Pattern: Playback engine

--- Apollo/Devices/Pattern.cs
+++ Apollo/Devices/Pattern.cs
@@ -18,20 +18,51 @@ namespace Apollo.Devices {
 
         public PatternWindow Window;
         
+        private List<Frame> _frames;
+        
         public override Device Clone() => new Pattern();
 
-        public Pattern(): base(DeviceIdentifier) {
-            
+        public Pattern(List<Frame> frames = null): base(DeviceIdentifier) {
+            _frames = frames?? new List<Frame>() {new Frame()};
         }
 
-        private void Tick(object info) {
-            //if (info.GetType() == typeof()) {
-                
-            //}
+        private void FireCourier(Signal n, Color color, byte index, int time) {
+            Courier courier = new Courier() {
+                Info = new Signal(n.Source, (byte)index, color, n.Layer, n.MultiTarget),
+                AutoReset = false,
+                Interval = time,
+            };
+            courier.Elapsed += Tick;
+            courier.Start();
+        }
+
+        private void Tick(object sender, EventArgs e) {
+            Courier courier = (Courier)sender;
+            courier.Elapsed -= Tick;
+            
+            MIDIExit?.Invoke((Signal)courier.Info);
         }
 
         public override void MIDIEnter(Signal n) {
+            if (_frames.Count > 0) {
+                for (int i = 0; i < _frames[0].Screen.Length; i++)
+                    if (_frames[0].Screen[i].Lit)
+                        MIDIExit?.Invoke(new Signal(n.Source, (byte)i, _frames[0].Screen[i].Clone(), n.Layer, n.MultiTarget));
+                
+                int time = _frames[0].Time;
 
+                for (int i = 1; i < _frames.Count; i++) {
+                    for (int j = 0; j < _frames[i].Screen.Length; j++)
+                        if (_frames[i].Screen[j] != _frames[i - 1].Screen[j])
+                            FireCourier(n, _frames[i].Screen[j].Clone(), (byte)j, time);
+
+                    time += _frames[i].Time;
+                }
+                
+                for (int i = 0; i < _frames[0].Screen.Length; i++)
+                    if (_frames[0].Screen[i].Lit)
+                        FireCourier(n, new Color(0), (byte)i, time);
+            }
         }
 
         public override void Dispose() {
