bc241ca5be68517d94aebb847cafd1e5f12ac3a3
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-04-19 15:43:50 +0200

    LaunchpadGrid: Implement Scaling

--- Apollo/Components/LaunchpadGrid.cs
+++ Apollo/Components/LaunchpadGrid.cs
@@ -1,4 +1,7 @@
-﻿using Avalonia.Controls;
+﻿using System;
+
+using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Shapes;
 using Avalonia.Markup.Xaml;
@@ -10,6 +13,7 @@ namespace Apollo.Components {
         private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
         
         UniformGrid Grid;
+        Path TopLeft, TopRight, BottomLeft, BottomRight;
         Shape ModeLight;
 
         public delegate void PadChangedEventHandler(int index);
@@ -26,12 +30,55 @@ namespace Apollo.Components {
             else ((Shape)Grid.Children[index]).Fill = color;
         }
 
+        private double _scale = 1;
+        public double Scale {
+            get => _scale;
+            set {
+                value = Math.Max(0, value);
+                if (value != _scale) {
+                    _scale = value;
+
+                    this.Resources["PadSquareSize"] = 15 * Scale;
+                    this.Resources["PadCircleSize"] = 11 * Scale;
+                    this.Resources["PadCut1"] = 3 * Scale;
+                    this.Resources["PadCut2"] = 12 * Scale;
+                    this.Resources["ModeWidth"] = 4 * Scale;
+                    this.Resources["ModeHeight"] = 2 * Scale;
+                    this.Resources["PadMargin"] = new Thickness(1 * Scale);
+                    this.Resources["ModeMargin"] = new Thickness(0, 5 * Scale, 0, 0);
+                    
+                    DrawPath();
+                }
+            }
+        }
+
+        public string FormatPath(string format) => String.Format(format,
+            this.Resources["PadSquareSize"],
+            this.Resources["PadCut1"],
+            this.Resources["PadCut2"]
+        );
+
+        public void DrawPath() {
+            TopLeft.Data = Geometry.Parse(FormatPath("M 0,0 L 0,{0} {2},{0} {0},{2} {0},0 Z"));
+            TopRight.Data = Geometry.Parse(FormatPath("M 0,0 L 0,{2} {1},{0} {0},{0} {0},0 Z"));
+            BottomLeft.Data = Geometry.Parse(FormatPath("M 0,0 L 0,{0} {0},{0} {0},{1} {2},0 Z"));
+            BottomRight.Data = Geometry.Parse(FormatPath("M 0,{1} L 0,{0} {0},{0} {0},0 {1},0 Z"));
+        }
+
         public LaunchpadGrid() {
             InitializeComponent();
 
             Grid = this.Get<UniformGrid>("LaunchpadGrid");
+
+            TopLeft = this.Get<Path>("TopLeft");
+            TopRight = this.Get<Path>("TopRight");
+            BottomLeft = this.Get<Path>("BottomLeft");
+            BottomRight = this.Get<Path>("BottomRight");
+
             ModeLight = this.Get<Rectangle>("ModeLight");
         }
+
+        private void LayoutChanged(object sender, EventArgs e) => DrawPath();
 
         bool mouseHeld = false;
 
