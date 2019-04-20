4849354bc8a088b158cc3710fe9b4d1d950aba4b
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-04-21 00:40:01 +0200

    PatternWindow: Implement Play Button

--- Apollo/Windows/PatternWindow.cs
+++ Apollo/Windows/PatternWindow.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Linq;
 
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Layout;
 using Avalonia.Markup.Xaml;
 using AvaloniaColor = Avalonia.Media.Color;
@@ -28,9 +30,18 @@ namespace Apollo.Windows {
         LaunchpadGrid Editor;
         Controls Contents;
         ColorPicker ColorPicker;
-        Dial Duration;
+        Dial Duration, Gate;
+        Button Play, Fire;
 
         int current;
+        bool _playing = false;
+        bool Playing {
+            get => _playing;
+            set {
+                _playing = value;
+                Duration.Enabled = Gate.Enabled = Play.IsEnabled = Fire.IsEnabled = !_playing;
+            }
+        }
         
         private void UpdateTitle(string path, int index) => this.Get<TextBlock>("Title").Text = (path == "")
             ? $"Editing Pattern - Track {index + 1}"
@@ -70,7 +81,12 @@ namespace Apollo.Windows {
             Editor = this.Get<LaunchpadGrid>("Editor");
 
             Duration = this.Get<Dial>("Duration");
-            this.Get<Dial>("Gate").RawValue = (double)_pattern.Gate * 100;
+            
+            Gate = this.Get<Dial>("Gate");
+            Gate.RawValue = (double)_pattern.Gate * 100;
+
+            Play = this.Get<Button>("Play");
+            Fire = this.Get<Button>("Fire");
 
             Contents = this.Get<StackPanel>("Frames").Children;
 
@@ -93,6 +109,8 @@ namespace Apollo.Windows {
         }
 
         private void Unloaded(object sender, EventArgs e) {
+            Playing = false;
+
             _pattern.Window = null;
 
             Launchpad.PatternWindow = null;
@@ -106,6 +124,8 @@ namespace Apollo.Windows {
         }
 
         private void Frame_Insert(int index) {
+            if (Playing) return;
+
             ((FrameDisplay)Contents[1]).Remove.Opacity = 1;
 
             _pattern.Frames.Insert(index, new Frame());
@@ -118,6 +138,8 @@ namespace Apollo.Windows {
         private void Frame_InsertStart() => Frame_Insert(0);
 
         private void Frame_Remove(int index) {
+            if (Playing) return;
+
             if (_pattern.Frames.Count == 1) return;
 
             if (index < current) current--;
@@ -130,6 +152,8 @@ namespace Apollo.Windows {
         }
 
         private void Frame_Select(int index) {
+            if (Playing) return;
+
             ((FrameDisplay)Contents[current + 1]).Viewer.Time.FontWeight = FontWeight.Normal;
 
             current = index;
@@ -147,6 +171,8 @@ namespace Apollo.Windows {
         }
 
         private void PadPressed(int index) {
+            if (Playing) return;
+
             int signalIndex = LaunchpadGrid.GridToSignal(index);
 
             _pattern.Frames[current].Screen[signalIndex] = (_pattern.Frames[current].Screen[signalIndex] == ColorPicker.Color)
@@ -162,6 +188,8 @@ namespace Apollo.Windows {
         }
 
         public void MIDIEnter(Signal n) {
+            if (Playing) return;
+
             if (n.Color.Lit) Dispatcher.UIThread.InvokeAsync(() => {
                 PadPressed(LaunchpadGrid.SignalToGrid(n.Index));
             });
@@ -180,6 +208,78 @@ namespace Apollo.Windows {
         }
 
         private void Gate_Changed(double value) => _pattern.Gate = (decimal)(value / 100);
+
+        private void FireCourier(Color color, byte index, int time) {
+            Courier courier = new Courier() {
+                Info = new Signal(Launchpad, (byte)index, color),
+                AutoReset = false,
+                Interval = time,
+            };
+            courier.Elapsed += Tick;
+            courier.Start();
+        }
+
+        private void FireStopCourier(int time) {
+            Courier courier = new Courier() {
+                AutoReset = false,
+                Interval = time,
+            };
+            courier.Elapsed += Tick;
+            courier.Start();
+        }
+
+        private void Tick(object sender, EventArgs e) {
+            if (!Playing) return;
+
+            Courier courier = (Courier)sender;
+            courier.Elapsed -= Tick;
+
+            if (courier.Info == null) {
+                Dispatcher.UIThread.InvokeAsync(() => {
+                    Playing = false;
+
+                    Editor.RenderFrame(_pattern.Frames[current]);
+                });
+
+                for (int i = 0; i < _pattern.Frames[current].Screen.Length; i++)
+                    Launchpad.Send(new Signal(Launchpad, (byte)i, _pattern.Frames[current].Screen[i]));
+
+            } else {
+                Signal n = (Signal)courier.Info;
+                
+                Dispatcher.UIThread.InvokeAsync(() => {
+                    Editor.SetColor(LaunchpadGrid.SignalToGrid(n.Index), (SolidColorBrush)n.Color.ToBrush());
+                });
+
+                Launchpad.Send(n);
+            }
+        }
+
+        private void PatternPlay(object sender, RoutedEventArgs e) {
+            if (Playing) return;
+            Playing = true;
+
+            Editor.RenderFrame(_pattern.Frames[0]);
+
+            for (int i = 0; i < _pattern.Frames[0].Screen.Length; i++)
+                Launchpad.Send(new Signal(Launchpad, (byte)i, _pattern.Frames[0].Screen[i]));
+            
+            decimal time = (_pattern.Frames[0].Mode? (int)_pattern.Frames[0].Length : _pattern.Frames[0].Time) * _pattern.Gate;
+
+            for (int i = 1; i < _pattern.Frames.Count; i++) {
+                for (int j = 0; j < _pattern.Frames[i].Screen.Length; j++)
+                    if (_pattern.Frames[i].Screen[j] != _pattern.Frames[i - 1].Screen[j])
+                        FireCourier(_pattern.Frames[i].Screen[j].Clone(), (byte)j, (int)time);
+
+                time += (_pattern.Frames[i].Mode? (int)_pattern.Frames[i].Length : _pattern.Frames[i].Time) * _pattern.Gate;
+            }
+                    
+            FireStopCourier((int)time);
+        }
+
+        private void PatternFire(object sender, RoutedEventArgs e) {
+
+        }
 
         private void MoveWindow(object sender, PointerPressedEventArgs e) => BeginMoveDrag();
         
