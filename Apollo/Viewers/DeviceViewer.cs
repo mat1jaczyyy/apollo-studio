5c38a96f8a862fcdedb14b894c555b30c759554b
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-04-24 22:00:36 +0200

    DeviceViewer: Go up to DropZone

--- Apollo/Viewers/DeviceViewer.cs
+++ Apollo/Viewers/DeviceViewer.cs
@@ -71,6 +71,8 @@ namespace Apollo.Viewers {
         private void Drop(object sender, DragEventArgs e) {
             IControl source = (IControl)e.Source;
 
+            while (source.Name != "DropZoneAfter" && source.Name != "DropZone") source = source.Parent;
+            
             if (source.Name != "DropZoneAfter" && e.GetPosition(source).X < source.Bounds.Width / 2) {
                 if (_device.ParentIndex == 0) ((Device)e.Data.Get(Device.Identifier)).Move(_device.Parent);
                 else ((Device)e.Data.Get(Device.Identifier)).Move(_device.Parent[_device.ParentIndex.Value - 1]);
