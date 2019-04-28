3eaf0e17a48e880e319daadd02673a3983d2ae09
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-04-28 03:30:19 +0200

    Tone: Saturation and Value Low/High

--- Apollo/DeviceViewers/ToneViewer.cs
+++ Apollo/DeviceViewers/ToneViewer.cs
@@ -17,15 +17,21 @@ namespace Apollo.DeviceViewers {
 
             _tone = tone;
 
-            this.Get<Dial>("Hue").RawValue = (double)_tone.Hue;
-            this.Get<Dial>("Saturation").RawValue = (double)_tone.Saturation * 100;
-            this.Get<Dial>("Value").RawValue = (double)_tone.Value * 100;
+            this.Get<Dial>("Hue").RawValue = _tone.Hue;
+
+            this.Get<Dial>("SaturationHigh").RawValue = _tone.SaturationHigh * 100;
+            this.Get<Dial>("SaturationLow").RawValue = _tone.SaturationLow * 100;
+            
+            this.Get<Dial>("ValueHigh").RawValue = _tone.ValueHigh * 100;
+            this.Get<Dial>("ValueLow").RawValue = _tone.ValueLow * 100;
         }
 
-        private void Hue_Changed(double value) => _tone.Hue = (int)value;
+        private void Hue_Changed(double value) => _tone.Hue = value;
 
-        private void Saturation_Changed(double value) => _tone.Saturation = (decimal)(value / 100);
+        private void SaturationHigh_Changed(double value) => _tone.SaturationHigh = value / 100;
+        private void SaturationLow_Changed(double value) => _tone.SaturationLow = value / 100;
 
-        private void Value_Changed(double value) => _tone.Value = (decimal)(value / 100);
+        private void ValueHigh_Changed(double value) => _tone.ValueHigh = value / 100;
+        private void ValueLow_Changed(double value) => _tone.ValueLow = value / 100;
     }
 }
--- Apollo/Devices/Tone.cs
+++ Apollo/Devices/Tone.cs
@@ -12,9 +12,9 @@ namespace Apollo.Devices {
     public class Tone: Device {
         public static readonly new string DeviceIdentifier = "tone";
 
-        private decimal _h, _s, _v;
+        private double _h, _sh, _sl, _vh, _vl;
 
-        public decimal Hue {
+        public double Hue {
             get => _h;
             set {
                 if (-180 <= value && value <= 180)
@@ -22,37 +22,57 @@ namespace Apollo.Devices {
             }
         }
 
-        public decimal Saturation {
-            get => _s;
+        public double SaturationHigh {
+            get => _sh;
             set {
-                if (0 <= value && value <= 4)
-                    _s = value;
+                if (0 <= value && value <= 1)
+                    _sh = value;
             }
         }
 
-        public decimal Value {
-            get => _v;
+        public double SaturationLow {
+            get => _sl;
             set {
-                if (0 <= value && value <= 4)
-                    _v = value;
+                if (0 <= value && value <= 1)
+                    _sl = value;
             }
         }
 
-        public override Device Clone() => new Tone(Hue, Saturation, Value);
+        public double ValueHigh {
+            get => _vh;
+            set {
+                if (0 <= value && value <= 1)
+                    _vh = value;
+            }
+        }
 
-        public Tone(decimal hue = 0, decimal saturation = 1, decimal value = 1): base(DeviceIdentifier) {
+        public double ValueLow {
+            get => _vl;
+            set {
+                if (0 <= value && value <= 1)
+                    _vl = value;
+            }
+        }
+
+        public override Device Clone() => new Tone(Hue, SaturationHigh, SaturationLow, ValueHigh, ValueLow);
+
+        public Tone(double hue = 0, double saturation_high = 1, double saturation_low = 0, double value_high = 1, double value_low = 0): base(DeviceIdentifier) {
             Hue = hue;
-            Saturation = saturation;
-            Value = value;
+
+            SaturationHigh = saturation_high;
+            SaturationLow = saturation_low;
+
+            ValueHigh = value_high;
+            ValueLow = value_low;
         }
 
         public override void MIDIEnter(Signal n) {
             if (n.Color.Lit) {
                 (double hue, double saturation, double value) = n.Color.ToHSV();
 
-                hue = (hue + (double)Hue) % 360;
-                saturation = Math.Min(1, saturation * (double)Saturation);
-                value = Math.Min(1, value * (double)value);
+                hue = (hue + Hue) % 360;
+                saturation = saturation * (SaturationHigh - SaturationLow) + SaturationLow;
+                value = value * (ValueHigh - ValueLow) + ValueLow;
 
                 n.Color = Color.FromHSV(hue, saturation, value);
             }
@@ -67,9 +87,11 @@ namespace Apollo.Devices {
             Dictionary<string, object> data = JsonConvert.DeserializeObject<Dictionary<string, object>>(json["data"].ToString());
             
             return new Tone(
-                Convert.ToDecimal(data["hue"].ToString()),
-                Convert.ToDecimal(data["saturation"].ToString()),
-                Convert.ToDecimal(data["value"].ToString())
+                Convert.ToDouble(data["hue"].ToString()),
+                Convert.ToDouble(data["saturation_high"].ToString()),
+                Convert.ToDouble(data["saturation_low"].ToString()),
+                Convert.ToDouble(data["value_high"].ToString()),
+                Convert.ToDouble(data["value_low"].ToString())
             );
         }
 
@@ -88,11 +110,17 @@ namespace Apollo.Devices {
                         writer.WritePropertyName("hue");
                         writer.WriteValue(Hue);
 
-                        writer.WritePropertyName("saturation");
-                        writer.WriteValue(Saturation);
+                        writer.WritePropertyName("saturation_high");
+                        writer.WriteValue(SaturationHigh);
 
-                        writer.WritePropertyName("value");
-                        writer.WriteValue(Value);
+                        writer.WritePropertyName("saturation_low");
+                        writer.WriteValue(SaturationLow);
+
+                        writer.WritePropertyName("value_high");
+                        writer.WriteValue(ValueHigh);
+
+                        writer.WritePropertyName("value_low");
+                        writer.WriteValue(ValueLow);
 
                     writer.WriteEndObject();
 
