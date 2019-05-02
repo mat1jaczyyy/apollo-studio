b59cc788a01b869f32f6915f9192993e4e296928
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-05-02 11:45:26 +0200

    Binary: Move version and id into Common

--- Apollo/Binary/Encoder.cs
+++ Apollo/Binary/Encoder.cs
@@ -9,50 +9,13 @@ using Apollo.Structures;
 
 namespace Apollo.Binary {
     public static class Encoder {
-        private const int version = 0;
-
-        public static readonly Type[] id = new Type[] {
-            typeof(Preferences),
-            typeof(ColorHistory),
-
-            typeof(Project),
-            typeof(Track),
-            typeof(Chain),
-            typeof(Device),
-            typeof(Launchpad),
-
-            typeof(Group),
-            typeof(Copy),
-            typeof(Delay),
-            typeof(Fade),
-            typeof(Flip),
-            typeof(Hold),
-            typeof(KeyFilter),
-            typeof(Layer),
-            typeof(Move),
-            typeof(Multi),
-            typeof(Output),
-            typeof(PageFilter),
-            typeof(PageSwitch),
-            typeof(Paint),
-            typeof(Pattern),
-            typeof(Preview),
-            typeof(Rotate),
-            typeof(Tone),
-
-            typeof(Color),
-            typeof(Frame),
-            typeof(Length),
-            typeof(Offset)
-        };
-
         private static void EncodeHeader(BinaryWriter writer) {
             writer.Write(new char[] {'A', 'P', 'O', 'L'});
-            writer.Write(version);
+            writer.Write(Common.version);
         }
 
         private static void EncodeID(BinaryWriter writer, Type type) {
-            writer.Write((byte)Array.IndexOf(id, type));
+            writer.Write((byte)Array.IndexOf(Common.id, type));
         }
 
         public static MemoryStream Encode(object o) {
