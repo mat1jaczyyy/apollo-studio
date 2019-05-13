7047d33fc7b636fe9617a41800f47e41eaeb13d4
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-05-14 01:51:12 +0200

    Implement UndoManager

---
+++ Apollo/Helpers/UndoManager.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Avalonia.Input;
+
+using Apollo.Core;
+
+namespace Apollo.Helpers {
+    public class UndoManager {
+        public class UndoEntry {
+            public readonly string Description;
+            public readonly Action Undo;
+            public readonly Action Redo;
+
+            public UndoEntry(string desc, Action undo = null, Action redo = null) {
+                Description = desc;
+                Undo = undo?? (() => {});
+                Redo = redo?? (() => {});
+            }
+        }
+
+        public List<UndoEntry> History = new List<UndoEntry>() {
+            new UndoEntry("Initial State")
+        };
+
+        public int Position { get; private set; } = 0;
+
+        public void Add(string desc, Action undo, Action redo) {
+            for (int i = Position + 1; i < History.Count; i++)
+                History.RemoveAt(i);
+            
+            History.Add(new UndoEntry(desc, undo, redo));
+            Position = History.Count - 1;
+        }
+
+        public void Select(int index) {
+            if (index < Position)
+                for (int i = Position; i > index; i--)
+                    History[i].Undo.Invoke();
+            
+            else if (index > Position)
+                for (int i = Position + 1; i <= index; i++)
+                    History[i].Redo.Invoke();
+            
+            Position = index;
+        }
+
+        public void Undo() => Select(Math.Min(0, Position - 1));
+        public void Redo() => Select(Math.Max(History.Count - 1, Position + 1));
+
+        public bool HandleKey(KeyEventArgs e) {
+            if (e.Modifiers == InputModifiers.Control) {
+                if (e.Key == Key.Z) {} // Undo
+                else if (e.Key == Key.Y) {} // Redo
+                else return false;
+
+            } else if (e.Modifiers == (InputModifiers.Control | InputModifiers.Shift)) {
+                if (e.Key == Key.Z) {} // Redo
+                else return false;
+            }
+
+            return true;
+        }
+    }
+}
