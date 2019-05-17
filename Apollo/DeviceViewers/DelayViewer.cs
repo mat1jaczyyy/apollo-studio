b16ceceac16d6d572e9cedf7dac37e5736c6eb64
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-05-17 23:38:40 +0200

    Delay Undoable

--- Apollo/DeviceViewers/DelayViewer.cs
+++ Apollo/DeviceViewers/DelayViewer.cs
@@ -1,8 +1,12 @@
-﻿using Avalonia.Controls;
+﻿using System.Collections.Generic;
+
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
 using Apollo.Components;
+using Apollo.Core;
 using Apollo.Devices;
+using Apollo.Elements;
 
 namespace Apollo.DeviceViewers {
     public class DelayViewer: UserControl {
@@ -11,23 +15,89 @@ namespace Apollo.DeviceViewers {
         private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
         
         Delay _delay;
+        Dial Duration, Gate;
 
         public DelayViewer(Delay delay) {
             InitializeComponent();
 
             _delay = delay;
-            Dial Duration = this.Get<Dial>("Duration");
+            Duration = this.Get<Dial>("Duration");
             Duration.UsingSteps = _delay.Mode;
             Duration.Length = _delay.Length;
             Duration.RawValue = _delay.Time;
 
-            this.Get<Dial>("Gate").RawValue = (double)_delay.Gate * 100;
+            Gate = this.Get<Dial>("Gate");
+            Gate.RawValue = (double)_delay.Gate * 100;
         }
 
-        private void Duration_Changed(double value) => _delay.Time = (int)value;
+        private void Duration_Changed(double value, double? old) {
+            if (old != null) {
+                int u = (int)old.Value;
+                int r = (int)value;
+                List<int> path = Track.GetPath(_delay);
 
-        private void Duration_ModeChanged(bool value) => _delay.Mode = value;
+                Program.Project.Undo.Add($"Delay Duration Changed", () => {
+                    ((Delay)Track.TraversePath(path)).Time = u;
+                }, () => {
+                    ((Delay)Track.TraversePath(path)).Time = r;
+                });
+            }
 
-        private void Gate_Changed(double value) => _delay.Gate = (decimal)(value / 100);
+            _delay.Time = (int)value;
+        }
+
+        public void SetDurationValue(int duration) => Duration.RawValue = duration;
+
+        private void Duration_ModeChanged(bool value, bool? old) {
+            if (old != null) {
+                bool u = old.Value;
+                bool r = value;
+                List<int> path = Track.GetPath(_delay);
+
+                Program.Project.Undo.Add($"Delay Duration Switched", () => {
+                    ((Delay)Track.TraversePath(path)).Mode = u;
+                }, () => {
+                    ((Delay)Track.TraversePath(path)).Mode = r;
+                });
+            }
+
+            _delay.Mode = value;
+        }
+
+        public void SetMode(bool mode) => Duration.UsingSteps = mode;
+
+        private void Duration_StepChanged(int value, int? old) {
+            if (old != null) {
+                int u = old.Value;
+                int r = value;
+                List<int> path = Track.GetPath(_delay);
+
+                Program.Project.Undo.Add($"Delay Duration Changed", () => {
+                    ((Delay)Track.TraversePath(path)).Length.Step = u;
+                }, () => {
+                    ((Delay)Track.TraversePath(path)).Length.Step = r;
+                });
+            }
+        }
+
+        public void SetDurationStep(int duration) => Duration.DrawArcAuto();
+
+        private void Gate_Changed(double value, double? old) {
+            if (old != null) {
+                decimal u = (decimal)(old.Value / 100);
+                decimal r = (decimal)(value / 100);
+                List<int> path = Track.GetPath(_delay);
+
+                Program.Project.Undo.Add($"Delay Gate Changed", () => {
+                    ((Delay)Track.TraversePath(path)).Gate = u;
+                }, () => {
+                    ((Delay)Track.TraversePath(path)).Gate = r;
+                });
+            }
+
+            _delay.Gate = (decimal)(value / 100);
+        }
+
+        public void SetGate(decimal gate) => Gate.RawValue = (double)gate * 100;
     }
 }
--- Apollo/Devices/Delay.cs
+++ Apollo/Devices/Delay.cs
@@ -1,5 +1,6 @@
 using System;
 
+using Apollo.DeviceViewers;
 using Apollo.Elements;
 using Apollo.Structures;
 
@@ -7,24 +8,45 @@ namespace Apollo.Devices {
     public class Delay: Device {
         public static readonly new string DeviceIdentifier = "delay";
 
-        public bool Mode; // true uses Length
-        public Length Length;
         private int _time;
-        private decimal _gate;
-
         public int Time {
             get => _time;
             set {
-                if (10 <= value && value <= 30000)
+                if (10 <= value && value <= 30000 && _time != value) {
                     _time = value;
+                    
+                    if (Viewer?.SpecificViewer != null) ((DelayViewer)Viewer.SpecificViewer).SetDurationValue(Time);
+                }  
             }
         }
 
+        private bool _mode; // true uses Length
+        public bool Mode {
+            get => _mode;
+            set {
+                if (_mode != value) {
+                    _mode = value;
+                    
+                    if (Viewer?.SpecificViewer != null) ((DelayViewer)Viewer.SpecificViewer).SetMode(Mode);
+                }
+            }
+        }
+
+        public Length Length;
+
+        private void LengthChanged() {
+            if (Viewer?.SpecificViewer != null) ((DelayViewer)Viewer.SpecificViewer).SetDurationStep(Length.Step);
+        }
+
+        private decimal _gate;
         public decimal Gate {
             get => _gate;
             set {
-                if (0.01M <= value && value <= 4)
+                if (0.01M <= value && value <= 4) {
                     _gate = value;
+                    
+                    if (Viewer?.SpecificViewer != null) ((DelayViewer)Viewer.SpecificViewer).SetGate(Gate);
+                }
             }
         }
 
@@ -35,6 +57,8 @@ namespace Apollo.Devices {
             Time = time;
             Length = length?? new Length();
             Gate = gate;
+
+            Length.Changed += LengthChanged;
         }
 
         private void Tick(object sender, EventArgs e) {
