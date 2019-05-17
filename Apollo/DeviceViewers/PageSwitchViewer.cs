afa34a46f86f5dae1a359eb610c96a1a6de80424
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-05-18 01:32:32 +0200

    PageSwitch Undoable

--- Apollo/DeviceViewers/PageSwitchViewer.cs
+++ Apollo/DeviceViewers/PageSwitchViewer.cs
@@ -1,8 +1,12 @@
-﻿using Avalonia.Controls;
+﻿using System.Collections.Generic;
+
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
 using Apollo.Components;
+using Apollo.Core;
 using Apollo.Devices;
+using Apollo.Elements;
 
 namespace Apollo.DeviceViewers {
     public class PageSwitchViewer: UserControl {
@@ -11,14 +15,33 @@ namespace Apollo.DeviceViewers {
         private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
         
         PageSwitch _pageswitch;
+        Dial Target;
 
         public PageSwitchViewer(PageSwitch pageswitch) {
             InitializeComponent();
 
             _pageswitch = pageswitch;
-            this.Get<Dial>("Target").RawValue = _pageswitch.Target;
+
+            Target = this.Get<Dial>("Target");
+            Target.RawValue = _pageswitch.Target;
         }
 
-        private void Target_Changed(double value) => _pageswitch.Target = (int)value;
+        private void Target_Changed(double value, double? old) {
+            if (old != null) {
+                int u = (int)old.Value;
+                int r = (int)value;
+                List<int> path = Track.GetPath(_pageswitch);
+
+                Program.Project.Undo.Add($"PageSwitch Target Changed", () => {
+                    ((PageSwitch)Track.TraversePath(path)).Target = u;
+                }, () => {
+                    ((PageSwitch)Track.TraversePath(path)).Target = r;
+                });
+            }
+
+            _pageswitch.Target = (int)value;
+        }
+
+        public void SetTarget(int value) => Target.RawValue = value;
     }
 }
