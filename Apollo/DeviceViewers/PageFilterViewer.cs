d14794b7a899b6642fb4ab8d29a7bc9d4de30fd2
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-05-18 02:40:21 +0200

    PageFilter Undoable

--- Apollo/DeviceViewers/PageFilterViewer.cs
+++ Apollo/DeviceViewers/PageFilterViewer.cs
@@ -1,12 +1,16 @@
-﻿using Avalonia;
+﻿using System.Collections.Generic;
+
+using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
+using UniformGrid = Avalonia.Controls.Primitives.UniformGrid;
 using Avalonia.Controls.Shapes;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using Avalonia.Input;
 
+using Apollo.Core;
 using Apollo.Devices;
+using Apollo.Elements;
 
 namespace Apollo.DeviceViewers {
     public class PageFilterViewer: UserControl {
@@ -32,7 +36,20 @@ namespace Apollo.DeviceViewers {
 
         private void Clicked(object sender, PointerReleasedEventArgs e) {
             int index = PagesGrid.Children.IndexOf((IControl)sender);
-            Set((Rectangle)sender, _filter[index] = !_filter[index]);
+
+            bool u = _filter[index];
+            bool r = !_filter[index];
+            List<int> path = Track.GetPath(_filter);
+
+            Program.Project.Undo.Add($"PageFilter Changed", () => {
+                ((PageFilter)Track.TraversePath(path))[index] = u;
+            }, () => {
+                ((PageFilter)Track.TraversePath(path))[index] = r;
+            });
+
+            _filter[index] = !_filter[index];
         }
+
+        public void Set(int index, bool value) => Set((Rectangle)PagesGrid.Children[index], value);
     }
 }
