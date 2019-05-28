91b88144ea6026921324eb92c56a71a514be11e5
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-05-28 23:18:33 +0200

    Implement ErrorWindow

---
+++ Apollo/Windows/ErrorWindow.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+using Apollo.Core;
+
+namespace Apollo.Windows {
+    public class ErrorWindow: Window {
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+        
+        private void UpdateTopmost(bool value) => Topmost = value;
+
+        public ErrorWindow(string message) {
+            InitializeComponent();
+            #if DEBUG
+                this.AttachDevTools();
+            #endif
+
+            UpdateTopmost(Preferences.AlwaysOnTop);
+            Preferences.AlwaysOnTopChanged += UpdateTopmost;
+
+            this.Get<TextBlock>("Message").Text = message;
+        }
+
+        private void Loaded(object sender, EventArgs e) {
+            foreach (Window window in Application.Current.Windows)
+                if (window != this)
+                    window.IsVisible = false;
+        }
+
+        private void Unloaded(object sender, EventArgs e) {
+            Preferences.AlwaysOnTopChanged -= UpdateTopmost;
+            
+            foreach (Window window in Application.Current.Windows)
+                if (window != this)
+                    window.IsVisible = true;
+        }
+
+        private void Window_Focus(object sender, PointerPressedEventArgs e) => this.Focus();
+
+        private void MoveWindow(object sender, PointerPressedEventArgs e) => BeginMoveDrag();
+
+        private void Close(object sender, RoutedEventArgs e) => Close();
+
+        public static void Create(string message, Window owner) {
+            ErrorWindow window = new ErrorWindow(message) {Owner = owner};
+            window.ShowDialog(owner);
+            window.Owner = null;
+        }
+    }
+}
