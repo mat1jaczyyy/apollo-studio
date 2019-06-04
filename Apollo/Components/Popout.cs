dffd74e26e59727165b74b34129948e84cfadd6b
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-06-05 01:40:40 +0200

    LaunchpadInfo: Implement real Launchpad popout

---
+++ Apollo/Components/Popout.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
+
+using Apollo.Windows;
+
+namespace Apollo.Components {
+    public class Popout: UserControl {
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+
+        public delegate void ClickedEventHandler();
+        public event ClickedEventHandler Clicked;
+
+        public Popout() => InitializeComponent();
+
+        private void Click(object sender, PointerReleasedEventArgs e) {
+            if (e.MouseButton == MouseButton.Left) Clicked?.Invoke();
+        }
+    }
+}
--- Apollo/Viewers/LaunchpadInfo.cs
+++ Apollo/Viewers/LaunchpadInfo.cs
@@ -1,13 +1,18 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
 
+using Apollo.Components;
 using Apollo.Elements;
+using Apollo.Windows;
 
 namespace Apollo.Viewers {
     public class LaunchpadInfo: UserControl {
         private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
         
         Launchpad _launchpad;
+
+        Popout Popout;
         ComboBox InputFormatSelector;
 
         public LaunchpadInfo(Launchpad launchpad) {
@@ -19,7 +24,16 @@ namespace Apollo.Viewers {
 
             InputFormatSelector = this.Get<ComboBox>("InputFormatSelector");
             InputFormatSelector.SelectedIndex = (int)_launchpad.InputFormat;
+
+            Popout = this.Get<Popout>("Popout");
+
+            if (_launchpad.GetType() == typeof(VirtualLaunchpad)) {
+                InputFormatSelector.IsEnabled = Popout.IsEnabled = false;
+                InputFormatSelector.Opacity = InputFormatSelector.Width = Popout.Opacity = Popout.Width = 0;
+            }
         }
+
+        private void Launchpad_Popout() => LaunchpadWindow.Create(_launchpad, (Window)this.GetVisualRoot());
 
         private void InputFormat_Changed(object sender, SelectionChangedEventArgs e) => _launchpad.InputFormat = (Launchpad.InputType)InputFormatSelector.SelectedIndex;
     }
