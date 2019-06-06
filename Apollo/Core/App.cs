f8c75956ee64b223316cfb006f49bdf20f8b43bf
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-06-06 17:10:03 +0200

    App: Experimental Light Theme

--- Apollo/Core/App.cs
+++ Apollo/Core/App.cs
@@ -1,8 +1,19 @@
-﻿using Avalonia;
+﻿using System;
+
+using Avalonia;
 using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.Styling;
 
 namespace Apollo.Core {
     public class App: Application {
-        public override void Initialize() => AvaloniaXamlLoader.Load(this);
+        public override void Initialize() {
+            AvaloniaXamlLoader.Load(this);
+
+            StyleInclude dark = new StyleInclude(new Uri("resm:Styles?assembly=Apollo")) {
+                Source = new Uri("avares://Apollo/Core/ThemeLight.xaml")
+            };
+
+            Styles.Add(dark);
+        }
     }
 }
