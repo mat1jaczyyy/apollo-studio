0131a4655ea09d0a1611ebdd7f690c63a58dfc4a
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-06-10 15:21:35 +0200

    Undo/RedoButton: Use IconButton
    Fixes #117.

--- Apollo/Components/RedoButton.cs
+++ Apollo/Components/RedoButton.cs
@@ -9,23 +9,27 @@ using Apollo.Core;
 using Apollo.Windows;
 
 namespace Apollo.Components {
-    public class RedoButton: UserControl {
+    public class RedoButton: IconButton {
         private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
 
-        private void Update_Position(int position) =>
-            this.Resources["CanvasBrush"] = (SolidColorBrush)Application.Current.Styles.FindResource((position == Program.Project.Undo.History.Count - 1)
-                ? "ThemeButtonDisabledBrush"
-                : "ThemeButtonEnabledBrush"
-            );
+        private void Update_Position(int position) => Enabled = position != Program.Project.Undo.History.Count - 1;
+
+        protected override IBrush Fill {
+            get => (IBrush)this.Resources["Brush"];
+            set => this.Resources["Brush"] = value;
+        }
 
         public RedoButton() {
             InitializeComponent();
+
+            AllowRightClick = true;
+            base.MouseLeave(this, null);
 
             Program.Project.Undo.PositionChanged += Update_Position;
             Update_Position(Program.Project.Undo.Position);
         }
 
-        private void Click(object sender, PointerReleasedEventArgs e) {
+        protected override void Click(PointerReleasedEventArgs e) {
             if (e.MouseButton == MouseButton.Left) Program.Project.Undo.Redo();
             else if (e.MouseButton == MouseButton.Right) UndoWindow.Create((Window)this.GetVisualRoot());
         }
