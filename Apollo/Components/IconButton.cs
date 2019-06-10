da1c79a7365fde6f01ba7373391f839c3769b55b
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-06-10 03:41:15 +0200

    Implement IconButton

---
+++ Apollo/Components/IconButton.cs
@@ -0,0 +1,44 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+namespace Apollo.Components {
+    public abstract class IconButton: UserControl {
+        public delegate void ClickedEventHandler();
+        public event ClickedEventHandler Clicked;
+
+        protected abstract IBrush Fill { get; set; }
+
+        private bool mouseHeld = false;
+
+        protected void MouseEnter(object sender, PointerEventArgs e) {
+            Fill = (IBrush)Application.Current.Styles.FindResource(mouseHeld? "ThemeButtonDownBrush" : "ThemeButtonOverBrush");
+        }
+
+        protected void MouseLeave(object sender, PointerEventArgs e) {
+            Fill = (IBrush)Application.Current.Styles.FindResource("ThemeButtonEnabledBrush");
+            mouseHeld = false;
+        }
+
+        protected void MouseDown(object sender, PointerPressedEventArgs e) {
+            if (e.MouseButton == MouseButton.Left) {
+                mouseHeld = true;
+
+                Fill = (IBrush)Application.Current.Styles.FindResource("ThemeButtonDownBrush");
+            }
+        }
+
+        protected void MouseUp(object sender, PointerReleasedEventArgs e) {
+            if (mouseHeld && e.MouseButton == MouseButton.Left) {
+                mouseHeld = false;
+
+                MouseEnter(sender, null);
+                Click();
+            }
+        }
+
+        protected virtual void Click() => Clicked?.Invoke();
+    }
+}
