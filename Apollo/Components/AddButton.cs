c2e86afd3f47e027ccf2c383638c557904fab079
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-06-25 17:30:54 +0200

    AddButton: Remove unused MouseOver

--- Apollo/Components/AddButton.cs
+++ Apollo/Components/AddButton.cs
@@ -31,16 +31,14 @@ namespace Apollo.Components {
         protected virtual void Unloaded(object sender, VisualTreeAttachmentEventArgs e) => Added = null;
 
         private bool mouseHeld = false;
-        private bool mouseOver = false;
 
         protected void MouseEnter(object sender, PointerEventArgs e) {
             Fill = (IBrush)Application.Current.Styles.FindResource(mouseHeld? "ThemeButtonDownBrush" : "ThemeButtonOverBrush");
-            mouseOver = true;
         }
 
         protected void MouseLeave(object sender, PointerEventArgs e) {
             Fill = (IBrush)Application.Current.Styles.FindResource("ThemeButtonEnabledBrush");
-            mouseHeld = mouseOver = false;
+            mouseHeld = false;
         }
 
         protected void MouseDown(object sender, PointerPressedEventArgs e) {
