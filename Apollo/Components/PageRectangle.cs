74ea2daeb14d6b76fd102d005d0ad6f3d66b3aec
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-06-30 02:01:53 +0200

    Implement PageRectangle

---
+++ Apollo/Components/PageRectangle.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+namespace Apollo.Components {
+    public class PageRectangle: UserControl {
+        private void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+
+            Rect = this.Get<Grid>("Rect");
+            Text = this.Get<TextBlock>("Text");
+        }
+
+        Grid Rect;
+        TextBlock Text;
+
+        public IBrush Fill {
+            get => Rect.Background;
+            set => Rect.Background = value;
+        }
+
+        public int Index {
+            set => Text.Text = value.ToString();
+        }
+
+        public PageRectangle() => InitializeComponent();
+    }
+}
