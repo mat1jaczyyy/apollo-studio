2a2b1c8f0e2a6daebc6c1500cadbb6cec4dc60c2
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-07-01 17:11:00 +0200

    SplashWindow: Learn Tab

--- Apollo/Windows/SplashWindow.cs
+++ Apollo/Windows/SplashWindow.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 using Avalonia;
@@ -126,6 +127,24 @@ namespace Apollo.Windows {
             if (result.Length > 0)
                 ReadFile(result[0]);
         }
+
+        void URL(string url) => Process.Start(new ProcessStartInfo() {
+            FileName = url,
+            UseShellExecute = true
+        });
+
+        public void Docs(object sender, RoutedEventArgs e)
+            => URL("https://github.com/mat1jaczyyy/apollo-studio/wiki");
+
+        public void Tutorials(object sender, RoutedEventArgs e) {}
+
+        public void Bug(object sender, RoutedEventArgs e)
+            => URL("https://github.com/mat1jaczyyy/apollo-studio/issues/new?assignees=mat1jaczyyy&labels=bug&template=bug_report.md&title=");
+
+        public void Feature(object sender, RoutedEventArgs e)
+            => URL("https://github.com/mat1jaczyyy/apollo-studio/issues/new?assignees=mat1jaczyyy&labels=enhancement&template=feature_request.md&title=");
+
+        public void Discord(object sender, RoutedEventArgs e) {}
 
         void MoveWindow(object sender, PointerPressedEventArgs e) => BeginMoveDrag();
     }
