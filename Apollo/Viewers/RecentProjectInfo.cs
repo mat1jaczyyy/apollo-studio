a8a09dbe9caa522e60cae5a58bbd1f738e53ed42
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-07-02 18:23:59 +0200

    RecentProjectInfo: Separate Filename and Folder

--- Apollo/Viewers/RecentProjectInfo.cs
+++ Apollo/Viewers/RecentProjectInfo.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia;
+﻿using System.IO;
+
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Markup.Xaml;
@@ -8,24 +10,44 @@ namespace Apollo.Viewers {
         void InitializeComponent() {
             AvaloniaXamlLoader.Load(this);
 
-            Path = this.Get<TextBlock>("Path");
+            Filename = this.Get<TextBlock>("Filename");
+            Folder = this.Get<TextBlock>("Folder");
         }
 
         public delegate void OpenedEventHandler(string path);
         public event OpenedEventHandler Opened;
         
-        TextBlock Path;
+        string _path;
+
+        TextBlock Filename, Folder;
 
         public RecentProjectInfo(string path) {
             InitializeComponent();
             
-            Path.Text = path;
+            _path = path;
+            
+            Filename.Text = Path.GetFileNameWithoutExtension(_path);
+            Folder.Text = $"({Path.GetFileName(Path.GetDirectoryName(_path))})";
         }
 
         void Unloaded(object sender, VisualTreeAttachmentEventArgs e) => Opened = null;
 
-        void Click(object sender, PointerReleasedEventArgs e) {
-            if (e.MouseButton == MouseButton.Left) Opened?.Invoke(Path.Text);
+        bool mouseHeld = false;
+
+        void MouseLeave(object sender, PointerEventArgs e) => mouseHeld = false;
+
+        void MouseDown(object sender, PointerPressedEventArgs e) {
+            if (e.MouseButton == MouseButton.Left || e.MouseButton == MouseButton.Right)
+                mouseHeld = true;
+        }
+
+        void MouseUp(object sender, PointerReleasedEventArgs e) {
+            if (mouseHeld) {
+                if (e.MouseButton == MouseButton.Left) Opened?.Invoke(_path);
+                else if (e.MouseButton == MouseButton.Right) {}
+
+                mouseHeld = false;
+            }
         }
     }
 }
