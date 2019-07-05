6b78f879add1515c0bbed6b3115a64e4ca9504b8
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-07-05 03:33:35 +0200

    macOS publishing script

---
+++ publish.sh
@@ -0,0 +1,34 @@
+cd Apollo
+dotnet publish -r osx.10.11-x64 -c Release
+sips -i icon.ico > /dev/null
+DeRez -only icns icon.ico > tmpicns.rsrc
+Rez -append tmpicns.rsrc -o bin/Release/netcoreapp2.2/osx.10.11-x64/publish/Apollo > /dev/null
+SetFile -a C bin/Release/netcoreapp2.2/osx.10.11-x64/publish/Apollo > /dev/null
+rm tmpicns.rsrc > /dev/null
+rm bin/Release/netcoreapp2.2/win10-x64/publish/Apollo.config > /dev/null 2>&1
+
+echo
+
+cd ../ApolloUpdate
+dotnet publish -r osx.10.11-x64 -c Release
+sips -i icon.ico > /dev/null
+DeRez -only icns icon.ico > tmpicns.rsrc
+Rez -append tmpicns.rsrc -o bin/Release/netcoreapp2.2/osx.10.11-x64/publish/ApolloUpdate >/dev/null
+SetFile -a C bin/Release/netcoreapp2.2/osx.10.11-x64/publish/ApolloUpdate >/dev/null
+rm tmpicns.rsrc > /dev/null
+
+echo
+echo Merging...
+
+cd ..
+rm -rf Build
+mkdir Build
+cd Build
+
+mkdir Apollo
+mkdir Update
+
+cp -r ../Apollo/bin/Release/netcoreapp2.2/osx.10.11-x64/publish/* Apollo
+cp -r ../ApolloUpdate/bin/Release/netcoreapp2.2/osx.10.11-x64/publish/* Update
+
+echo Done.
