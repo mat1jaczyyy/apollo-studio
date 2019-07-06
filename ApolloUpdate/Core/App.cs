b42328c7d6ca325b1ef824c3816f44462c492677
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-07-06 02:29:04 +0200

    ApolloUpdate: Remove Avalonia

--- ApolloUpdate/Core/App.cs
+++
@@ -1,8 +0,0 @@
-﻿using Avalonia;
-using Avalonia.Markup.Xaml;
-
-namespace Update.Core {
-    public class App: Application {
-        public override void Initialize() => AvaloniaXamlLoader.Load(this);
-    }
-}
--- ApolloUpdate/Program.cs
+++ ApolloUpdate/Program.cs
@@ -3,37 +3,36 @@ using System.IO;
 using System.IO.Compression;
 using System.Runtime.InteropServices;
 
-using Avalonia;
-using Avalonia.Logging.Serilog;
-
-using Update.Windows;
-
-namespace Update.Core {
+namespace ApolloUpdate {
     class Program {
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .LogToDebug();
+        public static string GetBaseFolder(string folder) => Path.Combine(
+            Directory.GetParent(
+                Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)
+            ).FullName,
+            folder
+        );
 
         static void Main(string[] args) {
             AppDomain.CurrentDomain.UnhandledException += (object sender, UnhandledExceptionEventArgs e) => {
-                string crashName = $"{AppDomain.CurrentDomain.BaseDirectory}crash-{DateTimeOffset.Now.ToUnixTimeSeconds()}";
+                string crashDir = GetBaseFolder("Crashes");
+
+                if (!Directory.Exists(crashDir)) Directory.CreateDirectory(crashDir);
+                string crashName = Path.Combine(crashDir, $"Crash-{DateTimeOffset.Now.ToUnixTimeSeconds()}");
 
                 using (var memoryStream = new MemoryStream()) {
-                    using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true)) {
+                    using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
                         using (Stream log = archive.CreateEntry("exception.log").Open())
                             using (StreamWriter writer = new StreamWriter(log))
                                 writer.Write(
                                     $"Operating System: {RuntimeInformation.OSDescription}\n\n" +
                                     e.ExceptionObject.ToString()
                                 );
-                    }
 
                     File.WriteAllBytes(crashName + ".zip", memoryStream.ToArray());
                 }
             };
             
-            BuildAvaloniaApp().Start<MainWindow>();
+            
         }
     }
 }
