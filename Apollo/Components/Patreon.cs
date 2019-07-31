6ba6f1221295846e551957b6a8e381906eb8f7a6
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-07-31 02:24:10 +0200

    Implement Patreon button

---
+++ Apollo/Components/Patreon.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+namespace Apollo.Components {
+    public class Patreon: IconButton {
+        void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+
+        protected override IBrush Fill {
+            get => (IBrush)this.Resources["Brush"];
+            set => this.Resources["Brush"] = value;
+        }
+
+        public Patreon() {
+            InitializeComponent();
+
+            base.MouseLeave(this, null);
+        }
+
+        protected override void Click(PointerReleasedEventArgs e) => Process.Start(new ProcessStartInfo() {
+            FileName = "https://patreon.com/mat1jaczyyy",
+            UseShellExecute = true
+        });
+    }
+}
