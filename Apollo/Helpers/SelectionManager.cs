1323933e85a6eb28168df4b30376d439698095c8
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-08-16 04:30:34 +0200

    SelectionManager: Rename via F2 key

--- Apollo/Helpers/SelectionManager.cs
+++ Apollo/Helpers/SelectionManager.cs
@@ -142,6 +142,7 @@ namespace Apollo.Helpers {
             } else {
                 if (e.Key == Key.Delete || e.Key == Key.Back) Action("Delete");
                 else if (e.Key == Key.D0 || e.Key == Key.NumPad0) Action("Mute");
+                else if (e.Key == Key.F2) Action("Rename");
                 else return false;
             }
 
