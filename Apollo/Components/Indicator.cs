4418b5536b8064c63a6d13b1590c4eec0fd8a105
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-08-30 03:22:30 +0200

    Implement Indicator

---
+++ Apollo/Components/Indicator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+
+using Apollo.Structures;
+
+namespace Apollo.Components {
+    public class Indicator: UserControl {
+        void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+
+            Display = this.Get<Ellipse>("Display");
+        }
+        
+        Ellipse Display;
+        Courier Timer;
+
+        void SetIndicator(bool state) => Dispatcher.UIThread.InvokeAsync(() => Display.Opacity = Convert.ToInt32(state));
+
+        public Indicator() => InitializeComponent();
+
+        void Unloaded(object sender, VisualTreeAttachmentEventArgs e) => Timer?.Dispose();
+
+        public void Trigger() {
+            Timer?.Dispose();
+
+            Timer = new Courier() {
+                Interval = 200
+            };
+
+            Timer.Elapsed += (_, __) => SetIndicator(false);
+            Timer.Start();
+
+            SetIndicator(true);
+        }
+    }
+}
