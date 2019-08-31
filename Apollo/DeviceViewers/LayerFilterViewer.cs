c46bfb488d2aa9de81a006c8fc60b2b81b501242
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-08-31 18:50:33 +0200

    Implement LayerFilterViewer

---
+++ Apollo/DeviceViewers/LayerFilterViewer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+using Apollo.Components;
+using Apollo.Core;
+using Apollo.Devices;
+using Apollo.Elements;
+using Apollo.Enums;
+
+namespace Apollo.DeviceViewers {
+    public class LayerFilterViewer: UserControl {
+        public static readonly string DeviceIdentifier = "layerfilter";
+
+        void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+
+            Target = this.Get<Dial>("Target");
+            Range = this.Get<Dial>("Range");
+        }
+        
+        LayerFilter _filter;
+        
+        Dial Target, Range;
+
+        public LayerFilterViewer(LayerFilter filter) {
+            InitializeComponent();
+
+            _filter = filter;
+            
+            Target.RawValue = _filter.Target;
+            Range.RawValue = _filter.Range;
+        }
+
+        void Unloaded(object sender, VisualTreeAttachmentEventArgs e) => _filter = null;
+
+        void Target_Changed(double value, double? old) {
+            if (old != null && old != value) {
+                int u = (int)old.Value;
+                int r = (int)value;
+                List<int> path = Track.GetPath(_filter);
+
+                Program.Project.Undo.Add($"Layer Target Changed to {r}{Target.Unit}", () => {
+                    ((LayerFilter)Track.TraversePath(path)).Target = u;
+                }, () => {
+                    ((LayerFilter)Track.TraversePath(path)).Target = r;
+                });
+            }
+
+            _filter.Target = (int)value;
+        }
+
+        public void SetTarget(int value) => Target.RawValue = value;
+
+        void Range_Changed(double value, double? old) {
+            if (old != null && old != value) {
+                int u = (int)old.Value;
+                int r = (int)value;
+                List<int> path = Track.GetPath(_filter);
+
+                Program.Project.Undo.Add($"Layer Filter Range Changed to {r}{Range.Unit}", () => {
+                    ((LayerFilter)Track.TraversePath(path)).Range = u;
+                }, () => {
+                    ((LayerFilter)Track.TraversePath(path)).Range = r;
+                });
+            }
+
+            _filter.Range = (int)value;
+        }
+
+        public void SetRange(int value) => Range.RawValue = value;
+    }
+}
