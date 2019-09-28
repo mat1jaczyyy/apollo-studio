b80c4b8f2a959da54eed10f9908f39bc567ee375
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-09-28 23:58:25 +0200

    Implement UpdateButton

---
+++ Apollo/Components/UpdateButton.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+namespace Apollo.Components {
+    public class UpdateButton: IconButton {
+        void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+
+            Path = this.Get<Path>("Path");
+        }
+
+        Path Path;
+
+        protected override IBrush Fill {
+            get => Path.Stroke;
+            set => Path.Stroke = value;
+        }
+
+        public UpdateButton() {
+            InitializeComponent();
+
+            base.MouseLeave(this, null);
+
+            Opacity = 0;
+            IsHitTestVisible = false;
+        }
+
+        public void Enable() {
+            Opacity = 1;
+            IsHitTestVisible = true;
+        }
+    }
+}
