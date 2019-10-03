9d4426b0ce46d3b2ba1842b7f38961174cbdc902
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-10-04 00:50:33 +0200

    Update to .NET Core 3 (#271)
    * Update to .NET Core 3
    * OSX Publish: Require 10.13 High Sierra, bump version
    * OSX Publish: Set icon with fileicon instead of Rez

--- Publish/publish.bat
+++ Publish/publish.bat
@@ -1,19 +1,19 @@
 @echo off
 
 cd ..\Apollo
-rd /S /Q bin\Release\netcoreapp2.2\win10-x64\publish
-dotnet publish -r win10-x64 -c Release
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.22.27905\bin\Hostx64\x64\editbin.exe" /subsystem:windows bin\Release\netcoreapp2.2\win10-x64\publish\Apollo.exe >nul 2>&1
-"C:\Users\mat1jaczyyy\Downloads\rcedit-x64.exe" --set-icon icon.ico bin\Release\netcoreapp2.2\win10-x64\publish\Apollo.exe
-del /s bin\Release\netcoreapp2.2\win10-x64\publish\Apollo.config >nul 2>&1
+rd /S /Q bin\Release\netcoreapp3.0\win-x64\publish
+dotnet publish -r win-x64 -c Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.22.27905\bin\Hostx64\x64\editbin.exe" /subsystem:windows bin\Release\netcoreapp3.0\win-x64\publish\Apollo.exe >nul 2>&1
+"C:\Users\mat1jaczyyy\Downloads\rcedit-x64.exe" --set-icon icon.ico bin\Release\netcoreapp3.0\win-x64\publish\Apollo.exe
+del /s bin\Release\netcoreapp3.0\win-x64\publish\Apollo.config >nul 2>&1
 
 echo.
 
 cd ..\ApolloUpdate
-rd /S /Q bin\Release\netcoreapp2.2\win10-x64\publish
-dotnet publish -r win10-x64 -c Release
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.22.27905\bin\Hostx64\x64\editbin.exe" /subsystem:windows bin\Release\netcoreapp2.2\win10-x64\publish\ApolloUpdate.exe >nul 2>&1
-"C:\Users\mat1jaczyyy\Downloads\rcedit-x64.exe" --set-icon icon.ico bin\Release\netcoreapp2.2\win10-x64\publish\ApolloUpdate.exe
+rd /S /Q bin\Release\netcoreapp3.0\win-x64\publish
+dotnet publish -r win-x64 -c Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.22.27905\bin\Hostx64\x64\editbin.exe" /subsystem:windows bin\Release\netcoreapp3.0\win-x64\publish\ApolloUpdate.exe >nul 2>&1
+"C:\Users\mat1jaczyyy\Downloads\rcedit-x64.exe" --set-icon icon.ico bin\Release\netcoreapp3.0\win-x64\publish\ApolloUpdate.exe
 
 echo.
 echo Merging...
@@ -27,10 +27,10 @@ mkdir Apollo
 mkdir M4L
 mkdir Update
 
-robocopy ..\Apollo\bin\Release\netcoreapp2.2\win10-x64\publish Apollo /E >nul 2>&1
+robocopy ..\Apollo\bin\Release\netcoreapp3.0\win-x64\publish Apollo /E >nul 2>&1
 robocopy ..\Apollo Apollo elevate.exe >nul 2>&1
 
-robocopy ..\ApolloUpdate\bin\Release\netcoreapp2.2\win10-x64\publish Update /E >nul 2>&1
+robocopy ..\ApolloUpdate\bin\Release\netcoreapp3.0\win-x64\publish Update /E >nul 2>&1
 robocopy ..\ApolloUpdate Update handle64.exe >nul 2>&1
 
 robocopy ..\M4L M4L *.amxd >nul 2>&1
--- Publish/publish.sh
+++ Publish/publish.sh
@@ -1,25 +1,17 @@
 #!/bin/sh
 
 cd ../Apollo
-rm -rf bin/Release/netcoreapp2.2/osx.10.11-x64/publish
-dotnet publish -r osx.10.11-x64 -c Release
-sips -i icon.ico > /dev/null
-DeRez -only icns icon.ico > tmpicns.rsrc
-Rez -append tmpicns.rsrc -o bin/Release/netcoreapp2.2/osx.10.11-x64/publish/Apollo > /dev/null
-SetFile -a C bin/Release/netcoreapp2.2/osx.10.11-x64/publish/Apollo > /dev/null
-rm tmpicns.rsrc > /dev/null
-rm bin/Release/netcoreapp2.2/win10-x64/publish/Apollo.config > /dev/null 2>&1
+rm -rf bin/Release/netcoreapp3.0/osx-x64/publish
+dotnet publish -r osx-x64 -c Release
+fileicon set bin/Release/netcoreapp3.0/osx-x64/publish/Apollo icon.ico
+rm bin/Release/netcoreapp3.0/osx-x64/publish/Apollo.config > /dev/null 2>&1
 
 echo
 
 cd ../ApolloUpdate
-rm -rf bin/Release/netcoreapp2.2/osx.10.11-x64/publish
-dotnet publish -r osx.10.11-x64 -c Release
-sips -i icon.ico > /dev/null
-DeRez -only icns icon.ico > tmpicns.rsrc
-Rez -append tmpicns.rsrc -o bin/Release/netcoreapp2.2/osx.10.11-x64/publish/ApolloUpdate >/dev/null
-SetFile -a C bin/Release/netcoreapp2.2/osx.10.11-x64/publish/ApolloUpdate >/dev/null
-rm tmpicns.rsrc > /dev/null
+rm -rf bin/Release/netcoreapp3.0/osx-x64/publish
+dotnet publish -r osx-x64 -c Release
+fileicon set bin/Release/netcoreapp3.0/osx-x64/publish/ApolloUpdate icon.ico
 
 echo
 echo Merging...
@@ -33,8 +25,8 @@ mkdir Apollo
 mkdir M4L
 mkdir Update
 
-cp -r ../Apollo/bin/Release/netcoreapp2.2/osx.10.11-x64/publish/* Apollo
-cp -r ../ApolloUpdate/bin/Release/netcoreapp2.2/osx.10.11-x64/publish/* Update
+cp -r ../Apollo/bin/Release/netcoreapp3.0/osx-x64/publish/* Apollo
+cp -r ../ApolloUpdate/bin/Release/netcoreapp3.0/osx-x64/publish/* Update
 cp ../M4L/*.amxd M4L
 
 echo Creating macOS Package...
