3be3d94fc657207505651024c24fe6c6cd7e20f1
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-10-31 00:21:42 +0100

    App: Raise Novation USB Driver requirement to 2.15.5

--- Apollo/Core/App.cs
+++ Apollo/Core/App.cs
@@ -30,6 +30,33 @@ namespace Apollo.Core {
 
         public static bool Dragging = false;
 
+        class DriverVersion: IComparable {
+            public int Minor { get; private set; }
+            public int Build { get; private set; }
+
+            public DriverVersion(int[] version) {
+                Minor = version[0];
+                Build = version[1];
+            }
+
+            public int CompareTo(object obj) {
+                DriverVersion that = (DriverVersion)obj;
+
+                if (this.Minor == that.Minor) {
+                    if (this.Build == that.Build) return 0;
+                    return (this.Build < that.Build)? -1 : 1;
+                }
+                
+                return (this.Minor < that.Minor)? -1 : 1;
+            }
+
+            public static bool operator <(DriverVersion a, DriverVersion b)
+                => a.CompareTo(b) == -1;
+
+            public static bool operator >(DriverVersion a, DriverVersion b) 
+                => a.CompareTo(b) == 1;
+        }
+
         public static bool WindowKey(Window sender, KeyEventArgs e) {
             if (e.KeyModifiers == ControlKey) {
                 if (e.Key == Key.W) sender.Close();
@@ -97,30 +124,32 @@ namespace Apollo.Core {
             if (Args.Length > 0 && Args[0] == "--update") lifetime.MainWindow = new UpdateWindow();
             else {
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) { // USB Driver check
-                    IEnumerable<int> a = (from j in Directory.GetDirectories(Environment.ExpandEnvironmentVariables(@"%SystemRoot%\System32\DriverStore\FileRepository\"))
+                    IEnumerable<DriverVersion> a = (from j in Directory.GetDirectories(Environment.ExpandEnvironmentVariables(@"%SystemRoot%\System32\DriverStore\FileRepository\"))
                         where Path.GetFileName(j).StartsWith("nvnusbaudio.inf")
-                        select Convert.ToInt32((
+                        select new DriverVersion((
                             from i in File.ReadAllLines(Path.Combine(j, "nvnusbaudio.inf"))
                                 where i.StartsWith("DriverVer=")
                                 select i
-                        ).First().Substring(10).Split(',')[1].Split('.')[1])
+                        ).First().Substring(10).Split(',')[1].Split('.').Where((x, i) => i == 1 || i == 3).Select(x => Convert.ToInt32(x)).ToArray())
                     );
 
                     if (a.Count() == 0) {
                         lifetime.MainWindow = new MessageWindow(
                             $"Apollo Studio requires the Novation USB Driver which isn't installed on your\n" +
                             "computer.\n\n" +
-                            "Please install at least version 2.7 of the driver before launching Apollo Studio."
+                            "Please install at least version 2.15.5 of the driver before launching Apollo\n" +
+                            "Studio."
                         );
                         base.OnFrameworkInitializationCompleted();
                         return;
                     }
 
-                    if (a.Max() < 7) {
+                    if (a.Max() < new DriverVersion(new int[] {15, 5})) {
                         lifetime.MainWindow = new MessageWindow(
                             $"Apollo Studio requires a newer version of the Novation USB Driver than is\n" +
                             "installed on your computer.\n\n" +
-                            "Please install at least version 2.7 of the driver before launching Apollo Studio."
+                            "Please install at least version 2.15.5 of the driver before launching Apollo\n" +
+                            "Studio."
                         );
                         base.OnFrameworkInitializationCompleted();
                         return;
