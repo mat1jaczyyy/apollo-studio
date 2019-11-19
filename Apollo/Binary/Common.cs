1cd7ee9f7a883c9e90d1f44646aa7153afb5fd18
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-11-19 02:59:35 +0100

    Binary: Bump file version

--- Apollo/Binary/Common.cs
+++ Apollo/Binary/Common.cs
@@ -8,7 +8,7 @@ using Apollo.Structures;
 
 namespace Apollo.Binary {
     public static class Common {
-        public const int version = 24;
+        public const int version = 25;
 
         public static readonly Type[] id = new Type[] {
             typeof(Preferences),
