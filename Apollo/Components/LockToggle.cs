12766a754f5bc5850f04ce7cf602a895c346cd0e
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-12-06 02:04:52 +0100

    Implement LockToggle

---
+++ Apollo/Components/LockToggle.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+namespace Apollo.Components {
+    public class LockToggle: IconButton {
+        void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+
+            Unlocked = this.Get<Canvas>("Unlocked");
+            Locked = this.Get<Canvas>("Locked");
+        }
+
+        Canvas Unlocked, Locked;
+
+        protected override IBrush Fill {
+            get => (IBrush)this.Resources["Brush"];
+            set => this.Resources["Brush"] = value;
+        }
+
+        public void SetState(bool value) =>
+            Unlocked.IsVisible = !(Locked.IsVisible = value);
+
+        public LockToggle() {
+            InitializeComponent();
+
+            base.MouseLeave(this, null);
+
+            SetState(false);
+        }
+    }
+}
