159de06412e439f8f158a142982d83b1ebe9c658
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-12-22 12:33:11 +0100

    FrameThumbnail: Change according to LaunchpadModel

--- Apollo/Components/FrameThumbnail.cs
+++ Apollo/Components/FrameThumbnail.cs
@@ -4,6 +4,8 @@ using Avalonia.Markup.Xaml;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
 
+using Apollo.Core;
+using Apollo.Enums;
 using Apollo.Structures;
 
 namespace Apollo.Components {
@@ -28,21 +30,43 @@ namespace Apollo.Components {
             }
         }
 
-        public FrameThumbnail() => InitializeComponent();
+        public FrameThumbnail() {
+            InitializeComponent();
+            
+            Preferences.LaunchpadModelChanged += Draw;
+        }
         
-        void Unloaded(object sender, VisualTreeAttachmentEventArgs e) => _frame = null;
+        void Unloaded(object sender, VisualTreeAttachmentEventArgs e) {
+            _frame = null;
+
+            Preferences.LaunchpadModelChanged -= Draw;
+        }
         
         public unsafe void Draw() {
             WriteableBitmap bitmap = new WriteableBitmap(new PixelSize(10, 10), new Vector(96, 96), PixelFormat.Rgba8888);
             
             using (ILockedFramebuffer l = bitmap.Lock()) {
                 uint* start = (uint*)l.Address;
-                
+
                 for (int y = 0; y < 10; y++) {
                     for (int x = 0; x < 10; x++) {
-                        if (x == 0 && y == 0 || x == 9 && y == 0 || x == 0 && y == 9 || x == 9 && y == 9) {} else {
-                            start[x + 10 * (9 - y)] = _frame.Screen[x + y * 10].ToUInt32();
+                        int i = y * 10 + x;
+                
+                        switch (Preferences.LaunchpadModel) {
+                            case LaunchpadModels.MK2:
+                                if (x == 0 || y == 0 || i == 99) continue;
+                                break;
+
+                            case LaunchpadModels.Pro:
+                                if (i == 0 || i == 9 || i == 90 || i == 99) continue;
+                                break;
+
+                            case LaunchpadModels.X:
+                                if (x == 0 || y == 0) continue;
+                                break;
                         }
+
+                        start[(9 - y) * 10 + x] = _frame.Screen[i].ToUInt32();
                     }
                 }
                 
