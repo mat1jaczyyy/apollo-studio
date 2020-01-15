795dd438756b52b01cfacf67ce0e7f68682dd1f8
mat1jaczyyy <mat1jaczyyy@gmail.com>
2020-01-15 13:58:50 +0100

    Refresh: Fix Clone

--- Apollo/Devices/Refresh.cs
+++ Apollo/Devices/Refresh.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 using Apollo.DeviceViewers;
 using Apollo.Elements;
 using Apollo.Structures;
@@ -5,18 +7,19 @@ using Apollo.Core;
 
 namespace Apollo.Devices {
     public class Refresh: Device {
-
-        public override Device Clone() => new Refresh() {
-            Collapsed = Collapsed,
-            Enabled = Enabled
-        };
-
         bool[] _macros = new bool[4];
+
         public bool GetMacro(int index) => _macros[index];
         public void SetMacro(int index, bool macro) {
             _macros[index] = macro;
+
             if (Viewer?.SpecificViewer != null) ((RefreshViewer)Viewer.SpecificViewer).SetMacro(index, macro);
         }
+        
+        public override Device Clone() => new Refresh(_macros.ToArray()) {
+            Collapsed = Collapsed,
+            Enabled = Enabled
+        };
 
         public Refresh(bool[] macros = null): base("refresh") {
             if (macros == null || macros.Length != 4) macros = new bool[4];
