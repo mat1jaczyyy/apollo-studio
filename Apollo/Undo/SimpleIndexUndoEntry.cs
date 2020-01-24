70e813acc0abb5965bbe758cf87ce77f8ec6c86a
mat1jaczyyy <mat1jaczyyy@gmail.com>
2020-01-24 10:49:16 +0100

    Implement SimpleIndexUndoEntry

---
+++ Apollo/Undo/SimpleIndexUndoEntry.cs
@@ -0,0 +1,16 @@
+using Apollo.Interfaces;
+
+namespace Apollo.Undo {
+    public abstract class SimpleIndexUndoEntry<T, I>: SimpleUndoEntry<T, I> where T: ISelect {
+        int index;
+
+        protected override void Action(T item, I element) => Action(item, index, element);
+        protected virtual void Action(T item, int index, I element) {}
+
+        protected override void Dispose(T item, I undo, I redo) => Dispose(item, index, undo, redo);
+        protected virtual void Dispose(T item, int index, I undo, I redo) {}
+
+        public SimpleIndexUndoEntry(string desc, T child, int index, I undo, I redo)
+        : base(desc, child, undo, redo) => this.index = index;
+    }
+}
