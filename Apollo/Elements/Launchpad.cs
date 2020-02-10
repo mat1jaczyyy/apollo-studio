b6d5775dbd6ee3c6d701bb25656157f577e2911b
mat1jaczyyy <mat1jaczyyy@gmail.com>
2020-02-10 17:38:56 +0100

    Revert "Launchpad: Add support for Mini MK3 Drum Rack mode"
    This reverts commit c492dafc3f7797829826940f7d1c1531a562fd9f.

--- Apollo/Elements/Launchpad.cs
+++ Apollo/Elements/Launchpad.cs
@@ -483,10 +483,8 @@ namespace Apollo.Elements {
         byte InputColor(int input) => (byte)(Math.Max(Convert.ToInt32(input > 0), input >> 1));
 
         void HandleNote(byte key, byte vel) {
-            if (IsGenerationX && InputFormat == InputType.DrumRack) {
-                if (108 <= key && key <= 115) key -= 80;
-                else if (key == 116) key = 27;
-            }
+            if (Type == LaunchpadType.X && InputFormat == InputType.DrumRack && 108 <= key && key <= 116)
+                key -= 80;
 
             HandleMessage(new Signal(
                 InputFormat,
