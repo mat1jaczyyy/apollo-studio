33cd9d5561562b27314e43e60cad1e2323563b19
mat1jaczyyy <mat1jaczyyy@gmail.com>
2020-04-28 02:59:10 +0200

    RtMidiDeviceInfo: Port uniqueness comment

--- Apollo/RtMidi/Unmanaged/Devices/Infos.cs
+++ Apollo/RtMidi/Unmanaged/Devices/Infos.cs
@@ -5,7 +5,15 @@ namespace Apollo.RtMidi.Unmanaged.Devices.Infos{
         internal RtMidiDeviceInfo(uint port, string name) {
             Port = port;
 
-            // RtMidi may add port number to end of name to ensure uniqueness
+            /*
+                RtMidi may add port number to "ensure uniqueness"
+                Disconnecting/reconnecting devices changes their port numbers, so it really can't ensure uniqueness
+                Additionally on Windows Microsoft GS Wavetable Synth always gets 0 so they always mismatch
+
+                https://github.com/micdah/rtmidi/blob/473ddc10aafd935d6d5d16e8876a566aa7c55a20/RtMidi.cpp#L2638-L2643
+
+                TODO Can we solve uniqueness on macOS?
+            */
             Name = name.EndsWith(port.ToString())
                 ? name.Substring(0, name.LastIndexOf(port.ToString(), StringComparison.Ordinal))
                 : name;
