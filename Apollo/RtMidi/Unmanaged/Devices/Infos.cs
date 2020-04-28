eb29281cb4a2bf970295f174ab906f5b51080812
mat1jaczyyy <mat1jaczyyy@gmail.com>
2020-04-28 05:54:12 +0200

    RtMidiDeviceInfo: Port uniqueness tested on macOS
    #249

--- Apollo/RtMidi/Unmanaged/Devices/Infos.cs
+++ Apollo/RtMidi/Unmanaged/Devices/Infos.cs
@@ -1,18 +1,26 @@
 using System;
 
-namespace Apollo.RtMidi.Unmanaged.Devices.Infos{
+namespace Apollo.RtMidi.Unmanaged.Devices.Infos {
     public class RtMidiDeviceInfo {
         internal RtMidiDeviceInfo(uint port, string name) {
             Port = port;
 
             /*
-                RtMidi may add port number to "ensure uniqueness"
-                Disconnecting/reconnecting devices changes their port numbers, so it really can't ensure uniqueness
-                Additionally on Windows Microsoft GS Wavetable Synth always gets 0 so they always mismatch
+                https://github.com/micdah/RtMidi.Core/issues/21
+                RtMidi adds port number on Windows to "ensure uniqueness".
+            
+                While it does succeed in ensuring it, disconnecting/reconnecting devices will change other devices'
+                port numbers, so now it's impossible to tell who's who.
+                
+                Additionally, on Windows, Microsoft GS Wavetable Synth is always assigned port number 0, so device
+                in/out port names will always mismatch. On macOS they at least match, but the issue above remains
+                unsolved.
 
+                It seems to go all the way to the OS' APIs, but Ableton Live somehow solved this... libusb?
+                
                 https://github.com/micdah/rtmidi/blob/473ddc10aafd935d6d5d16e8876a566aa7c55a20/RtMidi.cpp#L2638-L2643
 
-                TODO Can we solve uniqueness on macOS?
+                For now, remove the port number to ensure in/out names match, as it's more important.
             */
             Name = name.EndsWith(port.ToString())
                 ? name.Substring(0, name.LastIndexOf(port.ToString(), StringComparison.Ordinal))
