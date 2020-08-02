2a70f21bf2458d27b4b92fb4e9366e546c822836
mat1jaczyyy <mat1jaczyyy@gmail.com>
2020-08-03 00:54:34 +0200

    Screen: Fix Clearing not working

--- Apollo/Rendering/Screen.cs
+++ Apollo/Rendering/Screen.cs
@@ -64,10 +64,10 @@ namespace Apollo.Rendering {
         Pixel[] _screen = new Pixel[101];
 
         public void Clear() {
-            for (int i = 0; i < 101; i++) {
-                snapshot[i].Red = snapshot[i].Green = snapshot[i].Blue = 0;
-                _screen[i].Clear();
-            }
+            foreach (Pixel pixel in _screen)
+                pixel.Clear();
+            
+            Snapshot();
         }
 
         Color[] snapshot = new Color[101];
