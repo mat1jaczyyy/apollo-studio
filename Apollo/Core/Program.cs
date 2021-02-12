912daff7e2bce899ea1f574baac7dfc7919876c3
mat1jaczyyy <mat1jaczyyy@gmail.com>
2021-02-12 05:33:50 +0100

    Program: Bump version number

--- Apollo/Core/Program.cs
+++ Apollo/Core/Program.cs
@@ -15,7 +15,7 @@ using Apollo.Elements;
 
 namespace Apollo.Core {
     class Program {
-        public static readonly string Version = "Version 1.8.4";
+        public static readonly string Version = "Version 1.9.0";
 
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
--- Publish/Apollo.iss
+++ Publish/Apollo.iss
@@ -1,5 +1,5 @@
 #define MyAppName "Apollo Studio"
-#define MyAppVersion "1.8.4"
+#define MyAppVersion "1.9.0"
 #define MyAppPublisher "mat1jaczyyy"
 #define MyAppURL "apollo.mat1jaczyyy.com"
 #define MyAppExeName "Apollo.exe"
