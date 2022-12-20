54c3b671d4d5d43d154c63bdefff6c26336cab36
mat1jaczyyy <mat1jaczyyy@gmail.com>
2022-12-20 19:34:23 +0100

    Program: Bump version number

--- Apollo/Core/Program.cs
+++ Apollo/Core/Program.cs
@@ -15,7 +15,7 @@ using Apollo.Elements;
 
 namespace Apollo.Core {
     class Program {
-        public static readonly string Version = "Version 1.8.8";
+        public static readonly string Version = "Version 1.8.9";
 
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
