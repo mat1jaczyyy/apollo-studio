4b004a14d272e04e6273c6a3f5af9a8284149419
mat1jaczyyy <mat1jaczyyy@gmail.com>
2024-02-05 23:23:17 +0100

    PreferencesWindow: Add link to mat1jaczyyy website

---
+++ Apollo/Components/mat1jaczyyy.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+using Apollo.Core;
+
+namespace Apollo.Components {
+    public class mat1jaczyyy: IconButton {
+        public static readonly string URL = "https://mat1jaczyyy.com";
+
+        void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+
+        protected override IBrush Fill {
+            get => (IBrush)this.Resources["Brush"];
+            set => this.Resources["Brush"] = value;
+        }
+
+        public mat1jaczyyy() {
+            InitializeComponent();
+
+            base.MouseLeave(this, null);
+        }
+
+        protected override void Click(PointerReleasedEventArgs e) => App.URL(URL);
+    }
+}
